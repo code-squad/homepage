56d4650f2b6e558637c5ff8132c7daf88153eb69
jisuYoun <nigayoun@gmail.com>
2016-11-13 12:10:12 +0900

    change. vendors dir name

--- gulpfile.js
+++ gulpfile.js
@@ -52,13 +52,13 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy vendors libraries from /node_modules into /vendors
+// Copy libs libraries from /node_modules into /libs
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendors/bootstrap'))
+        .pipe(gulp.dest('libs/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendors/jquery'))
+        .pipe(gulp.dest('libs/jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -68,7 +68,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendors/font-awesome'))
+        .pipe(gulp.dest('libs/font-awesome'))
 })
 
 // Run everything
