717e6a9cb809d9dbae31cf613602d441ab17e337
jisuYoun <nigayoun@gmail.com>
2016-11-13 12:06:50 +0900

    change. vendor directory because of Github jekyll

--- gulpfile.js
+++ gulpfile.js
@@ -52,13 +52,13 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy vendor libraries from /node_modules into /vendor
+// Copy vendors libraries from /node_modules into /vendors
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('vendors/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('vendors/jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -68,7 +68,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('vendors/font-awesome'))
 })
 
 // Run everything
--- js/agency.js
+++ js/agency.js
@@ -1,33 +1,8 @@
-// Agency Theme JavaScript
-
-(function($) {
-    "use strict"; // Start of use strict
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    // $('a.page-scroll').bind('click', function(event) {
-    //     var $anchor = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    //     }, 1250, 'easeInOutExpo');
-    //     event.preventDefault();
-    // });
-
-    // Highlight the top nav as scrolling occurs
-    // $('body').scrollspy({
-    //     target: '.navbar-fixed-top',
-    //     offset: 51
-    // });
-
-    // Closes the Responsive Menu on Menu Item Click
-    // $('.navbar-collapse ul li a').click(function(){ 
-    //         $('.navbar-toggle:visible').click();
-    // });
-
-    // Offset for Main Navigation
-    // $('#mainNav').affix({
-    //     offset: {
-    //         top: 100
-    //     }
+(function() {
+    // document.addEventListener("DOMContentLoaded", function() {
+    //     "use strict"; 
+    //     var _urlPath = document.location.pathname.slice(1);
+    //     var elNavAnchor = document.querySelector("#mainNav .navbar-nav li>a[href='~"+_urlPath+"']");
+    //     if(elNavAnchor) elNavAnchor.style.color = "#0075d6";
     // })
-
-})(jQuery); // End of use strict
+})(); 
