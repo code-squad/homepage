7c242d4712478fb7abdbfdeccf42e2018ea46d41
jisuYoun <nigayoun@gmail.com>
2016-11-13 22:34:46 +0900

    add. scroll Animation for masters page

--- js/agency.js
+++ js/agency.js
@@ -1,8 +1,34 @@
 (function() {
-    // document.addEventListener("DOMContentLoaded", function() {
-    //     "use strict"; 
-    //     var _urlPath = document.location.pathname.slice(1);
-    //     var elNavAnchor = document.querySelector("#mainNav .navbar-nav li>a[href='~"+_urlPath+"']");
-    //     if(elNavAnchor) elNavAnchor.style.color = "#0075d6";
-    // })
+    "use strict"; 
+
+    function runScrollAnimation(targetTop) {
+      var STEP = 10;
+      var scrollValue=0;
+
+      function _move() {
+        if(!scrollValue)  scrollValue= STEP;
+        else scrollValue = STEP + scrollValue*1.10;
+
+        if(scrollValue < targetTop) { 
+          window.scrollTo(0, scrollValue);
+          window.requestAnimationFrame(_move);
+        } else {
+          window.scrollTo(0, targetTop);
+        }
+      }
+      window.requestAnimationFrame(_move);
+    }
+
+    document.querySelector(".course-kind").addEventListener("click", function(evt) {
+      evt.preventDefault();
+      if(evt.target.nodeName ==="BUTTON") {
+        var parentDiv = evt.target.parentElement.parentElement;
+        var parent = parentDiv.parentElement;
+        var index = Array.prototype.indexOf.call(parent.children, parentDiv);
+        var target = document.querySelectorAll("section.master-course")[index+1];
+        var targetTop = target.offsetTop;
+        runScrollAnimation(targetTop)
+      }
+    });
+
 })(); 
