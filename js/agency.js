b82735e298241807ca5e93349b20cb273b711d93
jisuYoun <nigayoun@gmail.com>
2016-11-14 12:02:53 +0900

    add. Sharedcto page created

--- js/agency.js
+++ js/agency.js
@@ -1,40 +1,45 @@
 (function() {
-    "use strict"; 
+  "use strict"; 
 
-    function runScrollAnimation(targetTop) {
-      var STEP = 10;
-      var scrollValue=0;
+  function runScrollAnimation(targetTop) {
+    var STEP = 10;
+    var scrollValue=0;
 
-      function _move() {
-        if(!scrollValue)  scrollValue= STEP;
-        else scrollValue = STEP + scrollValue*1.10;
+    function _move() {
+      if(!scrollValue)  scrollValue= STEP;
+      else scrollValue = STEP + scrollValue*1.10;
 
-        if(scrollValue < targetTop) { 
-          window.scrollTo(0, scrollValue);
-          window.requestAnimationFrame(_move);
-        } else {
-          window.scrollTo(0, targetTop);
-        }
+      if(scrollValue < targetTop) { 
+        window.scrollTo(0, scrollValue);
+        window.requestAnimationFrame(_move);
+      } else {
+        window.scrollTo(0, targetTop);
       }
-      window.requestAnimationFrame(_move);
     }
+    window.requestAnimationFrame(_move);
+  }
 
-    function getMatchedTargetOffsetTop(el) {
-      if(el.nodeName ==="BUTTON") {
-        var parentDiv = el.parentElement.parentElement;
-        var parent = parentDiv.parentElement;
-        var index = Array.prototype.indexOf.call(parent.children, parentDiv);
-        var target = document.querySelectorAll("section.master-course")[index+1];
-        var targetTop = target.offsetTop;
-      }
-      return targetTop;
+  function getMatchedTargetOffsetTop(el) {
+    if(el.nodeName ==="BUTTON") {
+      var parentDiv = el.parentElement.parentElement;
+      var parent = parentDiv.parentElement;
+      var index = Array.prototype.indexOf.call(parent.children, parentDiv);
+      var target = document.querySelectorAll("section.main-section")[index+1];
+      var targetTop = target.offsetTop;
     }
+    return targetTop;
+  }
 
-    document.querySelector(".course-kind").addEventListener("click", function(evt) {
+  function attachCourseKindEvents() {
+    elCourseKind.addEventListener("click", function(evt) {
       if(! window.requestAnimationFrame) return;
       evt.preventDefault();
       var targetTop = getMatchedTargetOffsetTop(evt.target);
       runScrollAnimation(targetTop);
     });
+  }
+
+  var elCourseKind =  document.querySelector(".scroll-animation-wrap");
+  if(elCourseKind) attachCourseKindEvents();
 
 })(); 
