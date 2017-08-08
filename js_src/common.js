fcc266a65a8cea7aa5be8f8aba3ccd0ea2bea4ec
jisuyoun <nigayoun@gmail.com>
2017-08-08 15:28:17 +0900

    refactor. remove jQuery and change modal javascript logic

---
+++ js_src/common.js
@@ -0,0 +1,53 @@
+/* Privacy and Term Modal Controller */
+(function() {
+	var state = "";
+	var elModal = document.querySelector("#myModal");
+	var elModalBody = document.querySelector("#myModal .modal-body");
+	var elModalLabel = document.querySelector("#myModalLabel");
+	var elPrivacy = document.querySelector("#codesquad-privacy");
+	var elTerm = document.querySelector("#codesquad-term");
+	var modalCloseBtn = document.querySelector(".modal-footer button");
+	var modalHeaderBtn = document.querySelector(".modal-header button");
+
+	function reqListener() {
+		elModalBody.innerHTML = this.responseText;
+		elModal.className += " in";
+		elModal.style.display = "block";
+		document.body.className += " modal-open";
+	}
+
+	function runXHR(url) {
+		var oReq = new XMLHttpRequest();
+		oReq.addEventListener("load", reqListener);
+		oReq.open("GET", url);
+		oReq.send();
+	}
+
+	//TODO. Refactoring.
+	elPrivacy.addEventListener("click", function(e) {
+		e.preventDefault();
+		//if (state === "privacy") return;
+		elModalLabel.innerText = this.textContent;
+		runXHR("../../../data/privacy.htm");
+		state = "privacy";
+	});
+
+	elTerm.addEventListener("click", function(e) {
+		e.preventDefault();
+		//if (state === "term") return;
+		elModalLabel.innerText = this.textContent;
+		runXHR("../data/term.htm");
+		state = "term";
+	});
+
+	function closeModalHandler(e) {
+		elModal.className = elModal.className.replace(/in/g, "");
+		document.body.className = document.body.className.replace(/modal\-open/g, "");
+		elModal.style.display="none";
+	}
+
+	modalCloseBtn.addEventListener("click", closeModalHandler);
+	modalHeaderBtn.addEventListener("click", closeModalHandler);
+
+
+})();
