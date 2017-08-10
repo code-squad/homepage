a2b0a76add10cfb3c28db5d5a39814b933c00f4f
jisuyoun <nigayoun@gmail.com>
2017-08-10 17:33:55 +0900

    add. schedule page (html/css) and redesign index layout

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var fileinclude = require('gulp-file-include');
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-    return gulp.src(['less/common.less', 'less/index.less', 'less/master.less', 'less/application.less'])
+    return gulp.src(['less/common.less', 'less/index.less', 'less/master.less', 'less/application.less', 'less/schedule.less'])
         .pipe(less())
         .pipe(concat('merged.css'))
         .pipe(header(banner, { pkg: pkg }))
