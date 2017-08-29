30a205e81f6fdc4d700330d8d8f831180d9e7e1c
jisuyoun <nigayoun@gmail.com>
2017-08-29 17:42:16 +0900

    add. navigation dropdown menu

--- js_src/common.js
+++ js_src/common.js
@@ -23,12 +23,48 @@
 		oReq.send();
 	}
 
-	//NAVIGATION
+	//1. NAVIGATION
 	var path = location.pathname;
 	var q = "a[href^='"+ path + "']";
 	document.querySelector(q).className = "selected-nav-menu";
 
-	//PRIVACE AND TERM Handler on Footer.
+
+	//2. NAVIGATION DROPDOWN
+	document.querySelector(".nav-masters > a").addEventListener("mouseenter", function(evt) {
+		const el = evt.target;
+		if(el.tagName !== "A") return;
+		else el.nextElementSibling.style.display = "block";
+	});
+
+	document.querySelector(".nav-masters > a").addEventListener("mouseleave", function(evt) {
+		function _isNavDropdownElement(elAfterOut) {
+			return (elAfterOut.className === "nav-dropdown" || elAfterOut.parentElement.classame === "nav-dropdown");
+		}
+
+		if(_isNavDropdownElement(evt.relatedTarget)) return;
+		evt.target.nextElementSibling.style.display = "none";
+
+
+	});
+
+	document.querySelector(".nav-dropdown").addEventListener("mouseleave", function(evt) {
+		const el = evt.target;
+		const name = el.tagName;
+
+		function _isMastersAnchorMenu(elAfterOut) {
+			return (elAfterOut.className === "nav-masters" || elAfterOut.parentElement.className === "nav-masters");
+		}
+
+		if(_isMastersAnchorMenu(evt.relatedTarget)) return;
+
+		if(name === "LI") el.parentElement.style.display = "none";
+		else if(name === "UL") el.style.display = "none";
+		else return;
+
+	});
+
+
+	//3. PRIVACE AND TERM Handler on Footer.
 	elPrivacy.addEventListener("click", function(e) {
 		e.preventDefault();
 		//if (state === "privacy") return;
