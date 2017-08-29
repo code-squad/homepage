838a42d0262e874bd0852d8c8bc5476070d184f1
jisuyoun <nigayoun@gmail.com>
2017-08-29 13:01:42 +0900

    change. level background according to click tab

--- js_src/level.js
+++ js_src/level.js
@@ -1,7 +1,7 @@
 (function(doc) {
 	//change background of card, when resize viewport.
 
-	const deteatWidth = {
+	const detectWidth = {
 		mobile : 606,
 		pc : 2280
 	}
@@ -25,35 +25,72 @@
 		}
 	}
 
-	function detechMediaChangeTiming() {
-		const mobileMedia = window.matchMedia('(max-width: '+ deteatWidth.mobile +'px)');
-		const pcMedia = window.matchMedia('(max-width: '+ deteatWidth.pc +'px)');
+
+	function changeBackgroundColor(ruleNumber) {
 		const ulSelector = "#masters-level .level-section-wrap > section > ul > li:nth-child"
-
-		function changeBackgroundColor(ruleNumber) {
-			Object.keys(rule).forEach((color) => {
-				rule[color].colorSet[ruleNumber].forEach((v) => {
-					doc.querySelector(ulSelector + '(' + v +')').style.backgroundColor = rule[color].rgb;
+		Object.keys(rule).forEach((color) => {
+			rule[color].colorSet[ruleNumber].forEach((v) => {
+				doc.querySelector(ulSelector + '(' + v +')').style.backgroundColor = rule[color].rgb;
+					//when ul exist, reset it's border color
+					const ulOfDesc = doc.querySelector(ulSelector + '(' + v + ') ul');
+					if(ulOfDesc) ulOfDesc.style.border = "20px solid" + rule[color].rgb;
 				})
-			});
-		}
+		});
+	}
 
+	function changeBGColorAfterResize() {
+		const mobileMedia = window.matchMedia('(max-width: '+ detectWidth.mobile +'px)');
+		const pcMedia = window.matchMedia('(max-width: '+ detectWidth.pc +'px)');
 		mobileMedia.addListener(function(evt) {
-			//mobile smaller
-			if(evt.matches) changeBackgroundColor('one');
-			//mobile larger
-			else changeBackgroundColor('two');
+			if(evt.matches) {
+			 changeBackgroundColor('one');
+			} else { 
+				changeBackgroundColor('two');
+			}
 		});
 
 		pcMedia.addListener(function(evt) {
-			//pc smaller
 			if(evt.matches) changeBackgroundColor('two');
-			//pc larger
 			else changeBackgroundColor('three');
 		});
 	}
 
-	//RUN
-	detechMediaChangeTiming();
+	function changeBGColorAfterDOMLoaded() {
+		doc.addEventListener("DOMContentLoaded", () => {
+			const windowWidth = window.innerWidth;
+			if(windowWidth <= detectWidth.mobile) {
+				changeBackgroundColor('one');
+			} else if(windowWidth > detectWidth.mobile && windowWidth < detectWidth.pc) {
+				changeBackgroundColor('two');
+			} else {
+				changeBackgroundColor('three');
+			}
+		});
+	}
 
+	//RUN
+	changeBGColorAfterResize();
+	changeBGColorAfterDOMLoaded();
 })(document);
+
+
+(function(doc) {
+//Select level by TabUI
+
+const ul = doc.querySelector("#masters-level nav ul");
+	ul.addEventListener("click", (evt) => {
+		const el = evt.target;
+		if(el.tagName !== "LI") return;
+		const idx = [].indexOf.call(el.parentElement.children, el) + 1;
+
+		//remove before block element
+		const visualSection = doc.querySelector(".level-section-wrap > section:not(.view-none)");
+		visualSection.classList.add("view-none");
+
+		//add selected element
+		const levelSection = doc.querySelector(".level-section-wrap > section:nth-child("+ idx +")");
+		levelSection.classList.remove("view-none");
+
+	});
+})(document)
+
