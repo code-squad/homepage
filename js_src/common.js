de75ce16929a90029c89064b817486e571161a14
jisuyoun <nigayoun@gmail.com>
2017-12-14 18:20:29 +0900

    change. tdd페이지노출, 종료아이콘 레이아웃 버그수정. special메인페이지 모바일크기 header조정

--- js_src/common.js
+++ js_src/common.js
@@ -32,7 +32,7 @@
 
 
 	//2. NAVIGATION DROPDOWN
-	/*
+	
 	var elDropdownlist = document.querySelector(".dropdown-list > a");
 	elDropdownlist.addEventListener("mouseenter", function(evt) {
 		const el = evt.target;
@@ -66,7 +66,7 @@
 		else return;
 
 	});
-	*/
+	
 
 
 	//3. PRIVACE AND TERM Handler on Footer.
