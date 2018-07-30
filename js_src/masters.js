0fbcf7a5e1b4a6a0732954a6cc9956b1ef2a8e5a
jisuyoun <nigayoun@gmail.com>
2018-07-30 16:19:22 +0900

    fix. special page faq event미등록

--- js_src/masters.js
+++ js_src/masters.js
@@ -1,31 +1,3 @@
-//FAQ
-(function() {
-	var faqWrap  = document.querySelector("#masters-faq ul");
-
-	//execute accodian
-	var faqWrap  = document.querySelector("#masters-faq ul");
-	faqWrap.addEventListener("click", function(e) {
-		var target = e.target;
-		if(target.className !== "question") return;
-		var answerStyle =  target.nextElementSibling.style;
-
-		if(parseInt(answerStyle.maxHeight) > 0) { 
-			answerStyle.maxHeight = 0;
-		} else  {
-			[].slice.call(faqWrap.querySelectorAll(".answer")).forEach(function(v){
-				if(parseInt(v.style.maxHeight) > 0) {
-					v.style.maxHeight = 0;
-					//v.parentElement.style.backgroundColor = '#fff';
-				}
-			});
-			setTimeout(function(){
-				answerStyle.maxHeight = "500px";
-				//target.parentElement.style.backgroundColor = 'rgb(124, 170, 203)';
-			}, 300)
-		}
-	});
-})();
-
 //등록 URL 변경
 (function() {
 	var urls = {
