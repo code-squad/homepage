aeae04085513e0da83bb4455ad297ea9a0f852a8
crong <crongro@gmail.com>
2020-10-19 20:48:00 +0900

    update. jQuery3.5.1

--- gulpfile.js
+++ gulpfile.js
@@ -174,20 +174,20 @@ gulp.task('default', gulp.parallel('minify-css', 'minify-js', 'mv-to-pages'));
 
 
 // // Copy libs libraries from /node_modules into /libs
-// gulp.task('copy', function() {
-//     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-//         .pipe(gulp.dest('libs/bootstrap'))
+gulp.task('copy-libs', function() {
+    // gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+    //     .pipe(gulp.dest('libs/bootstrap'))
 
-//     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-//         .pipe(gulp.dest('libs/jquery'))
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('libs/jquery'))
 
-//     gulp.src([
-//             'node_modules/font-awesome/**',
-//             '!node_modules/font-awesome/**/*.map',
-//             '!node_modules/font-awesome/.npmignore',
-//             '!node_modules/font-awesome/*.txt',
-//             '!node_modules/font-awesome/*.md',
-//             '!node_modules/font-awesome/*.json'
-//         ])
-//         .pipe(gulp.dest('libs/font-awesome'))
-// })
+    // gulp.src([
+    //         'node_modules/font-awesome/**',
+    //         '!node_modules/font-awesome/**/*.map',
+    //         '!node_modules/font-awesome/.npmignore',
+    //         '!node_modules/font-awesome/*.txt',
+    //         '!node_modules/font-awesome/*.md',
+    //         '!node_modules/font-awesome/*.json'
+    //     ])
+    //     .pipe(gulp.dest('libs/font-awesome'))
+ })
