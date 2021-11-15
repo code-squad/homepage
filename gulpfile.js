8b40b8f4774b5e83b7f17456155aea153a086b67
jisuyoun <nigayoun@gmail.com>
2021-11-15 17:29:15 +0900

    add.android 과정 페이지, ivy 추가, 2022년 masters일정 추가

--- gulpfile.js
+++ gulpfile.js
@@ -109,8 +109,8 @@ gulp.task('htmlinclude-reg', function() {
 
 gulp.task('htmlinclude-masters', function() {
   return gulp.src([
-      'html_src/masters/fe.html', 'html_src/masters/be.html','html_src/masters/ios.html',
-      'html_src/masters/curriculum.html', 'html_src/masters/cocoa.html'])
+      'html_src/masters/fe.html', 'html_src/masters/be.html','html_src/masters/ios.html','html_src/masters/android.html',
+      'html_src/masters/curriculum.html' ])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
