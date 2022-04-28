83955fe319d634fc1dbee6d2f877f73376898b04
Angelo <valrentin.angelo@gmail.com>
2022-04-28 12:19:25 +0900

    :lipstick: Typhography 테마 파일 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,66 @@
+const fontSize = {
+  display: {
+    lg: "5.6rem",
+    md: "4.8rem",
+    sm: "4rem",
+  },
+  body: {
+    xl: "2.6rem",
+    lg: "2rem",
+    md: "1.6rem",
+    sm: "1.4rem",
+    xs: "1.2rem",
+  },
+};
+
+const lineHeight = {
+  display: {
+    lg: "8.2rem",
+    md: "7rem",
+    sm: "5.8rem",
+  },
+  body: {
+    xl: "3.8rem",
+    lg: "3rem",
+    md: "2.6rem",
+    sm: "2.4rem",
+    xs: "1.8rem",
+  },
+};
+
+const letterSpacing = "-0.008em";
+
+const color = {
+  greyScale: {
+    black: "#1B1B1B",
+    grey1: "#3F3F3F",
+    grey2: "#777777",
+    grey3: "#C4C4C4",
+    offWhite: "#FAFAFA",
+    white: "#FFFFFF",
+  },
+  primary: {
+    green1: "#57A620",
+    green2: "#6DD028",
+    green3: "#8AD953",
+    green4: "#B4E791",
+    orange1: "#CC7210",
+    orange2: "#FF8E14",
+    orange3: "#FFA543",
+    orange4: "#FFC587",
+  },
+  secondary: {
+    blue1: "#1379C4",
+    blue2: "#8FDFF3",
+    blue3: "##C7E8EF",
+  },
+};
+
+const theme = {
+  fontSize,
+  lineHeight,
+  letterSpacing,
+  color,
+};
+
+export default theme;
