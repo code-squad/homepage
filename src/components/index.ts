feaff130f219e5cc34002b3fe691c19b23c4f659
Angelo <valrentin.angelo@gmail.com>
2022-04-28 18:45:38 +0900

    :sparkles: Footer 컴포넌트 제작
    모든 페이지의 최하단에 위치하게되는 Footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as SButton } from "./SButton";
 export { default as HomeGlobalNavigationBar } from "./HomeGlobalNavigationBar";
 export { default as TeamGlobalNavigationBar } from "./TeamGlobalNavigationBar";
+export { default as Footer } from "./Footer";
