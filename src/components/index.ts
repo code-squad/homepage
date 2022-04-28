5bc55b7c8ee74209ebde3695610af2d6a1848ca9
Angelo <valrentin.angelo@gmail.com>
2022-04-28 18:44:45 +0900

    :sparkles: TeamGlobalNavigationBar 제작
    팀 관련 페이지들에서 사용되는 네비게이션 바

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as SButton } from "./SButton";
 export { default as HomeGlobalNavigationBar } from "./HomeGlobalNavigationBar";
+export { default as TeamGlobalNavigationBar } from "./TeamGlobalNavigationBar";
