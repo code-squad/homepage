ecfb5ab929c9a2263a0a13360eb53eb0a0146b42
Angelo <valrentin.angelo@gmail.com>
2022-04-29 16:03:51 +0900

    :truck: 글로벌 컴포넌트를 각각의 폴더를 만들어 이동
    테스트 코드가 추가되거나 스타일링이 분리되면 components 폴더가 복잡해지기때문

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-export { default as SButton } from "./SButton";
-export { default as HGlobalNavigationBar } from "./HGlobalNavigationBar";
-export { default as TGlobalNavigationBar } from "./TGlobalNavigationBar";
-export { default as Footer } from "./Footer";
+export { SButton } from "./SButton";
+export { HGlobalNavigationBar } from "./HGlobalNavigationBar";
+export { TGlobalNavigationBar } from "./TGlobalNavigationBar";
+export { Footer } from "./Footer";
 export { Avatar } from "./Avatar";
 // export { InterviewBox } from "./InterviewBox";
