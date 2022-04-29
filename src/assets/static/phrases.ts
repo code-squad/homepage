0c434983cacbe63a53921d52054548a8af7e6ef1
Angelo <valrentin.angelo@gmail.com>
2022-04-29 14:27:56 +0900

    :sparkles: 컴포넌트에서 사용될 문구들이 정의되어있는 파일 추가

---
+++ src/assets/static/phrases.ts
@@ -0,0 +1,24 @@
+const HNAVIGATIONBAR = {
+  MASTERS: "마스터즈 코스",
+  CODE_TOGETHER: "코드투게더",
+  FAQ: "자주 묻는 질문",
+  SUBSCRIBE: "소식받아보기",
+};
+
+const TNAVIGATIONBAR = {
+  TEAM_CULTURE: "팀 문화",
+  RECRUIT: "채용소식",
+};
+
+const FOOTER = {
+  COMPANY_NAME: "코드스쿼드",
+  COMPANY_CEO_NAME: "대표자:김정",
+  COMPANY_ADDRESS: "주소:강남구 역삼동 836-24 역삼빌딩 4층",
+  COMPANY_REGISTRATION_NUMBER: "사업자번호:676-88-00504",
+  COMPANY_TEL_NUMBER: "전화:070-4117-1005",
+  COMPANY_EMAIL_ADDRESS: "yoda@codesquad.kr",
+  PRIVACY_POLICY: "개인정보 취급방침",
+  REFUND_POLICY: "환불규정",
+};
+
+export { HNAVIGATIONBAR, TNAVIGATIONBAR, FOOTER };
