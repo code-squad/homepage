918c87b55355225e03b38058703e30d4e0d3c47f
HTMLhead <mrdoo309@gmail.com>
2022-04-29 13:12:27 +0900

    :label: 에러가 나는 children타입 수정

--- src/lib/context/GlobalTheme.tsx
+++ src/lib/context/GlobalTheme.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "styled-components";
 import theme from "../../styles/theme";
 import GlobalStyle from "../../styles/GlobalStyle";
 
-const GlobalTheme = ({ children }: JSX.ElementChildrenAttribute) => (
+const GlobalTheme = ({ children }: { children: React.ReactNode }) => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     {children}
