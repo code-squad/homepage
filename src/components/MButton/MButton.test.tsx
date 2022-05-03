e144ab5e7c55440fd3fa2b6ee3ab069d6066e8e4
Angelo <valrentin.angelo@gmail.com>
2022-05-03 19:11:04 +0900

    :test_tube: MButton 테스트 코드 제작

---
+++ src/components/MButton/MButton.test.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { render } from "@testing-library/react";
+// Testing-Component
+import { MButton } from ".";
+// Assets
+import { LINK } from "assets/static/phrases";
+// Libs
+import { TestProvider } from "lib/testUtils";
+import { INTERNAL } from "assets/static/urls";
+
+describe("<MButton>", () => {
+  const props = {
+    accent: false,
+    disabled: false,
+    to: INTERNAL.SUBSCRIBE,
+  };
+  const renderMButton = (buttonText: string) =>
+    render(
+      <TestProvider>
+        <MButton {...props}>{buttonText}</MButton>
+      </TestProvider>
+    );
+  it("버튼 문구가 보여진다.", () => {
+    const { getByText } = renderMButton(LINK.SUBSCRIBE);
+
+    getByText(LINK.SUBSCRIBE);
+  });
+  it("버튼을 클릭하면 props로 전달된곳으로 페이지가 이동된다.", () => {
+    const { getByRole } = renderMButton(LINK.SUBSCRIBE);
+
+    const linkEle = getByRole("link", { name: LINK.SUBSCRIBE });
+    expect(linkEle?.getAttribute("href")).toBe(INTERNAL.SUBSCRIBE);
+  });
+});
