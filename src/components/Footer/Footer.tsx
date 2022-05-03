f13c53de197237f255c9b7ad79f185dd83de91a2
Angelo <valrentin.angelo@gmail.com>
2022-05-03 19:07:22 +0900

    :recycle: 하드코딩되어있던 path 및 url을 static url을 사용하도록 변경

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,9 @@ import styled from "styled-components";
 // Typography
 import { XSBody } from "typography/";
 // Static
-import { LINK, MESSAGE } from "assets/static/phrases";
+import { BUTTON, LINK, MESSAGE } from "assets/static/phrases";
+import { INTERNAL, EXTERNAL } from "assets/static/urls";
+import { Link } from "gatsby";
 
 const Footer: React.FC = () => {
   return (
@@ -15,12 +17,22 @@ const Footer: React.FC = () => {
           </div>
           <CompanyInfomationWrapper>
             <div>
-              <XSBody>{`${MESSAGE.COMPANY_CEO_NAME} | ${MESSAGE.COMPANY_REGISTRATION_NUMBER}`}</XSBody>
-              <XSBody>{`${MESSAGE.COMPANY_ADDRESS} | ${MESSAGE.COMPANY_TEL_NUMBER}`}</XSBody>
-              <XSBody>{MESSAGE.COMPANY_EMAIL_ADDRESS}</XSBody>
+              <div>
+                <XSBody as="span">{`${MESSAGE.COMPANY_CEO_NAME}`}</XSBody>
+                <XSBody as="span"> | </XSBody>
+                <XSBody as="span">{`${MESSAGE.COMPANY_REGISTRATION_NUMBER}`}</XSBody>
+              </div>
+              <div>
+                <XSBody as="span">{`${MESSAGE.COMPANY_ADDRESS}`}</XSBody>
+                <XSBody as="span"> | </XSBody>
+                <XSBody as="span">{`${MESSAGE.COMPANY_TEL_NUMBER}`}</XSBody>
+                <XSBody>{MESSAGE.COMPANY_EMAIL_ADDRESS}</XSBody>
+              </div>
             </div>
             <div>
-              <XSBody bold>{`${LINK.PRIVACY_POLICY} | ${LINK.REFUND_POLICY}`}</XSBody>
+              <XSBody as="span">{`${BUTTON.PRIVACY_POLICY}`}</XSBody>
+              <XSBody as="span"> | </XSBody>
+              <XSBody as="span">{`${BUTTON.REFUND_POLICY}`}</XSBody>
             </div>
           </CompanyInfomationWrapper>
         </CompanyWrapper>
@@ -35,17 +47,19 @@ const Footer: React.FC = () => {
               <XSBody bold>{MESSAGE.CURRICULUM}</XSBody>
             </li>
             <li>
-              <Button href="https://codesquad.kr/masters">{LINK.MASTERS}</Button>
+              <InternalLinkButton to={INTERNAL.MASTERS}>{LINK.MASTERS}</InternalLinkButton>
             </li>
             <li>
-              <Button href="https://codesquad.kr/code-together">{LINK.CODE_TOGETHER}</Button>
+              <InternalLinkButton to={INTERNAL.CODE_TOGETHER}>
+                {LINK.CODE_TOGETHER}
+              </InternalLinkButton>
             </li>
           </MenuList>
           <MenuList>
             <li>
-              <Button bold href="https://codesquad.kr/faq">
+              <InternalLinkButton $bold to={INTERNAL.FAQ}>
                 {LINK.FAQ}
-              </Button>
+              </InternalLinkButton>
             </li>
           </MenuList>
           <MenuList>
@@ -53,22 +67,19 @@ const Footer: React.FC = () => {
               <XSBody bold>{MESSAGE.SOCIAL_MEDIA}</XSBody>
             </li>
             <li>
-              <Button href="https://codesquad-yoda.medium.com/" target="_blank">
+              <ExternalLinkButton href={EXTERNAL.BLOG} target="_blank">
                 {LINK.BLOG}
-              </Button>
+              </ExternalLinkButton>
             </li>
             <li>
-              <Button
-                href="https://www.youtube.com/channel/UC8OU76dfIn8jvWmXt8roMZg"
-                target="_blank"
-              >
+              <ExternalLinkButton href={EXTERNAL.YOUTUBE} target="_blank">
                 {LINK.YOUTUBE}
-              </Button>
+              </ExternalLinkButton>
             </li>
             <li>
-              <Button href="https://www.facebook.com/codesquad.kr/" target="_blank">
+              <ExternalLinkButton href={EXTERNAL.FACEBOOK} target="_blank">
                 {LINK.FACEBOOK}
-              </Button>
+              </ExternalLinkButton>
             </li>
           </MenuList>
         </MenuListWrapper>
@@ -125,7 +136,20 @@ const MenuList = styled.ul`
   gap: 1.6rem 0;
 `;
 
-const Button = styled.a<{ bold?: boolean }>`
+const InternalLinkButton = styled(Link)<{ $bold?: boolean }>`
+  color: ${({ theme: { color } }) => color.greyScale.white};
+  font-size: ${({ theme: { fontSize } }) => fontSize.body.xs};
+  font-weight: ${({ $bold, theme: { fontWeight } }) =>
+    $bold ? fontWeight.medium : fontWeight.regular};
+  line-height: ${({ theme: { lineHeight } }) => lineHeight.body.xs};
+  letter-spacing: ${({ theme: { letterSpacing } }) => letterSpacing};
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ExternalLinkButton = styled.a<{ bold?: boolean }>`
   color: ${({ theme: { color } }) => color.greyScale.white};
   font-size: ${({ theme: { fontSize } }) => fontSize.body.xs};
   font-weight: ${({ bold, theme: { fontWeight } }) =>
