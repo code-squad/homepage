77f13ade4df2655e056cd950986a92fd778b1d86
Angelo <valrentin.angelo@gmail.com>
2022-05-10 13:20:32 +0900

    :lipstick: disabled 여부에 따라 컬러 및 cursor 옵션이 설정되도록 변경
    태그가 button에서 Link으로 변경되어서 &:disabled 선택자가 동작하지 않게되어 수정하게됨

--- src/components/SButton/SButton.tsx
+++ src/components/SButton/SButton.tsx
@@ -9,35 +9,35 @@ export interface ISButtonProps {
 }
 
 const SButton: React.FC<ISButtonProps> = ({ children, disabled, to }) => {
-  return <SButtonWrapper {...{ disabled, to }}>{children}</SButtonWrapper>;
+  return (
+    <SButtonWrapper $disabled={disabled} {...{ to }}>
+      {children}
+    </SButtonWrapper>
+  );
 };
 
-const SButtonWrapper = styled(Link)`
-  display: block;
+const SButtonWrapper = styled(Link)<{ $disabled?: boolean }>`
+  display: inline-block;
   height: 3.2rem;
   padding: 0 0.8rem;
-  color: ${({ theme: { color } }) => color.greyScale.white};
+  color: ${({ $disabled, theme: { color } }) =>
+    $disabled ? color.greyScale.white20 : color.greyScale.white};
   font-size: ${({ theme: { fontSize } }) => fontSize.body.xs};
   font-weight: ${({ theme: { fontWeight } }) => fontWeight.medium};
   line-height: 3.2rem;
   letter-spacing: ${({ theme: { letterSpacing } }) => letterSpacing};
-  border-color: ${({ theme: { color } }) => color.greyScale.black};
+  border-color: ${({ $disabled, theme: { color } }) =>
+    $disabled ? color.greyScale.grey2 : color.greyScale.black};
   border-width: 0;
   border-radius: 0.8rem;
-  background-color: ${({ theme: { color } }) => color.greyScale.black};
+  background-color: ${({ $disabled, theme: { color } }) =>
+    $disabled ? color.greyScale.grey2 : color.greyScale.black};
   text-decoration: none;
-  &:hover {
-    cursor: pointer;
-  }
+  cursor: pointer;
+  pointer-events: ${({ $disabled }) => ($disabled ? "none" : "auto")};
   &:active {
     background-color: ${({ theme: { color } }) => color.greyScale.grey1};
     border-color: ${({ theme: { color } }) => color.greyScale.grey1};
-  }
-  &:disabled {
-    cursor: auto;
-    color: ${({ theme: { color } }) => color.greyScale.white20};
-    background-color: ${({ theme: { color } }) => color.greyScale.grey2};
-    border-color: ${({ theme: { color } }) => color.greyScale.grey2};
   }
 `;
 
