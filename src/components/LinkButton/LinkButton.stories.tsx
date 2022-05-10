2160e6017453d24cbaa6c2d8d1e75dc1a8b8ee06
HTMLhead <mrdoo309@gmail.com>
2022-05-10 16:31:11 +0900

    :white_check_mark: linkButton 스토리 추가

---
+++ src/components/LinkButton/LinkButton.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+// Type
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+// Story-Component
+import { LinkButton } from ".";
+// Assets
+import mastersIcon from "assets/images/icons/masters.svg";
+
+export default {
+  title: "LinkButton",
+  component: LinkButton,
+} as ComponentMeta<typeof LinkButton>;
+
+const Template: ComponentStory<typeof LinkButton> = (args) => <LinkButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  description: "코드스쿼드의 정규 교육 과정",
+  title: "마스터즈 코스",
+  to: "https://codesquad.kr",
+};
+
+export const Icon = Template.bind({});
+Icon.args = {
+  description: "코드스쿼드의 정규 교육 과정",
+  title: "마스터즈 코스",
+  to: "https://codesquad.kr",
+  icon: mastersIcon,
+};
