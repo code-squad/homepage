ea3a58537b36014c65bca5204d490b6f864b7023
HTMLhead <mrdoo309@gmail.com>
2022-05-10 16:03:36 +0900

    :white_check_mark: 짧은 dropdown스토리 추가

--- src/components/DropdownItem/DropdownItem.stories.tsx
+++ src/components/DropdownItem/DropdownItem.stories.tsx
@@ -44,3 +44,12 @@ DropdownList.args = {
   content: "마스터즈 코스는 온오프라인 공간을 모두 활용해 학습하는 것을 지향합니다.",
   editDate: "2019-05-04",
 };
+
+export const ShortDropdown = Template.bind({});
+ShortDropdown.args = {
+  category: "교육과정",
+  title: "코드스쿼드는 온라인 수업만 진행하나요?",
+  content: "마스터즈 코스는 온오프라인 공간을 모두 활용해 학습하는 것을 지향합니다.",
+  editDate: "2019-05-04",
+  short: true,
+};
