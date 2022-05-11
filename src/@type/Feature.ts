4bf8a2dbed6ec9053d14ec818543a204e7413018
Angelo <valrentin.angelo@gmail.com>
2022-05-11 18:44:55 +0900

    :label: 메인 화면에서 사용되는 쿼리 결과에 대한 타입들을 추가

---
+++ src/@type/Feature.ts
@@ -0,0 +1,5 @@
+export interface FeatureType {
+  title?: string;
+  subtitle?: string;
+  description?: string;
+}
---
+++ src/@type/Master.ts
@@ -0,0 +1,7 @@
+export interface MasterType {
+  field?: string;
+  name?: string;
+  description?: string;
+  introduce?: string;
+  careers?: string[];
+}
