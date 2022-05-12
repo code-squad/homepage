a42a83ff9afb0eae4a2c2766d9cc814e81f240a8
Angelo <valrentin.angelo@gmail.com>
2022-05-12 17:17:00 +0900

    :lipstick: 최소 크기가 유지되도록 변경, 높이값을 고정시키지 않도록 변경

--- src/components/InterviewBox/InterviewBox.tsx
+++ src/components/InterviewBox/InterviewBox.tsx
@@ -43,10 +43,9 @@ const InterviewBox: React.FC<IInterviewBox> = ({
 };
 
 const InterviewBoxWrapper = styled.div`
-  padding: 4.8rem;
+  padding: 4.8rem 4.8rem 7rem 4.8rem;
   background-color: ${({ theme: { color } }) => color.greyScale.offWhite};
-  width: 42.3rem;
-  height: 35.8rem;
+  min-width: 42.3rem;
 `;
 
 const Nutshell = styled.div`
@@ -83,4 +82,5 @@ const WriterInfo = styled.span`
   display: inline-block;
   color: ${({ theme: { color } }) => color.greyScale.grey2};
 `;
+
 export default InterviewBox;
