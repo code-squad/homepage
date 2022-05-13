5eefffb4a0581cdd74df21bb3a002a1635fdaf35
HTMLhead <mrdoo309@gmail.com>
2022-05-13 13:18:41 +0900

    :label: subject, curriculum, masterinfo타입 추가

--- src/@type/Master.ts
+++ src/@type/Master.ts
@@ -1,7 +1,17 @@
+import images from "assets/images";
+
 export interface MasterType {
   field: string;
   name: string;
   description: string;
   introduce: string;
   careers: string[];
+}
+
+export interface MasterInfoType {
+  avatar: keyof typeof images;
+  name: string;
+  position: string;
+  introduce: string;
+  nutshell: string;
 }
