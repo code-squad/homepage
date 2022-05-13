53bbac3bca9040a6686932d04a85cdfbc7a96e8e
HTMLhead <mrdoo309@gmail.com>
2022-05-13 17:42:57 +0900

    :sparkles: 졸업생 후기 컴포넌트 추가

--- src/pages/masters/index.tsx
+++ src/pages/masters/index.tsx
@@ -5,6 +5,7 @@ import GlobalTheme from "lib/context/GlobalTheme";
 // Typography
 import { XSBody } from "typography/";
 // Components
+import { Interview } from "components/Interview";
 // Assets
 
 // Libs
@@ -12,6 +13,7 @@ import { HomeGlobalNavigationBar } from "components/";
 import { Masthead } from "pageComponents/masters/Masthead";
 import { EducationFeatures } from "pageComponents/masters/EducationFeatures";
 import { DetailCurriculum } from "pageComponents/masters/DetailCurriculum";
+import { InterviewSliderWrapper } from "pageComponents/masters/InterviewSliderWrapper";
 
 const MatsersPage: React.FC<PageProps> = ({ data }) => {
   return (
@@ -21,6 +23,7 @@ const MatsersPage: React.FC<PageProps> = ({ data }) => {
         <Masthead />
         <EducationFeatures />
         <DetailCurriculum />
+        <InterviewSliderWrapper />
       </main>
     </GlobalTheme>
   );
