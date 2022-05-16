334f40cf53a2cae1560841c23f63b1cf04acfad0
Angelo <valrentin.angelo@gmail.com>
2022-05-16 12:17:40 +0900

    :label: 곧 시작하는 과정에 대한 속성 추가

--- src/@type/Master.ts
+++ src/@type/Master.ts
@@ -1,7 +1,16 @@
+import icons from "assets/images/icons";
+
+interface ScheduleType {
+  title: string;
+  subtitle: string;
+  image: keyof typeof icons;
+}
+
 export interface MasterType {
   field: string;
   name: string;
   description: string;
   introduce: string;
   careers: string[];
+  schedules?: ScheduleType[];
 }
