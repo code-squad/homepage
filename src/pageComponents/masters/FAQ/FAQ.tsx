51a2cdd82663b18bc93e0eca2730558881041666
HTMLhead <mrdoo309@gmail.com>
2022-05-17 12:41:11 +0900

    :sparkles: FAQ컴포넌트 제작

--- src/pageComponents/masters/FAQ/FAQ.tsx
+++ src/pageComponents/masters/FAQ/FAQ.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import styled from "styled-components";
-
+import { graphql, useStaticQuery } from "gatsby";
+// Type
+import { FAQType } from "@type/FAQ";
 // Components
-import { DropdownItem, SectionTitle } from "components";
+import { DropdownItem, MButton, SectionTitle } from "components";
 // Assets
 import { SUBTITLE, TITLE } from "assets/static/phrases";
+// Utils
+import { strainMdxInfo } from "lib/utils";
 
-const FAQ: React.FC = ({}) => {
+const FAQ: React.FC = () => {
+  const { lists } = strainMdxInfo(useStaticQuery(FAQListQuery));
+
+  const mastersFAQList = lists.filter((list: FAQType) => list.category === "교육과정");
+
   return (
     <FAQWrapper>
       <SectionTitle subTitle={SUBTITLE.FAQ} title={TITLE.FREQUENTLY_ASKED_QUESTIONS} />
       <DropdownWrapper>
-        <DropdownItem
-          category="교육과정"
-          title="코드스쿼드는 온라인 수업만 진행하나요?"
-          content="네"
-        />
-        <DropdownItem
-          category="교육과정"
-          title="코드스쿼드는 온라인 수업만 진행하나요?"
-          content="네"
-        />
+        {mastersFAQList.map(({ category, title, content, editDate }: FAQType) => (
+          <DropdownItemWrapper key={title}>
+            <DropdownItem {...{ category, title, content, editDate }} />
+          </DropdownItemWrapper>
+        ))}
       </DropdownWrapper>
+      <MButton children="더보기" to="/faq" />
     </FAQWrapper>
   );
 };
@@ -34,7 +38,28 @@ const FAQWrapper = styled.div`
   min-width: 144rem;
   flex-direction: column;
 `;
-const DropdownWrapper = styled.div`
+const DropdownWrapper = styled.ul`
   margin-top: 8rem;
+  margin-bottom: 2.4rem;
 `;
+const DropdownItemWrapper = styled.li`
+  margin-bottom: 4rem;
+`;
+
+const FAQListQuery = graphql`
+  query FAQListQuery {
+    mdx(frontmatter: { templateKey: { eq: "faq_lists" } }) {
+      frontmatter {
+        lists {
+          content
+          editDate
+          index
+          title
+          category
+        }
+      }
+    }
+  }
+`;
+
 export default FAQ;
