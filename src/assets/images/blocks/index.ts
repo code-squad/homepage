c2a3fbf97b2e2fff9dc95d55a048c79fddd34b7d
Angelo <valrentin.angelo@gmail.com>
2022-05-19 17:51:58 +0900

    :bento: 블록 이미지로 사용될 이미지들을 추가

---
+++ src/assets/images/blocks/index.ts
@@ -0,0 +1,19 @@
+import bookSupport from "./book-support.svg";
+import culturalSupport from "./cultural-support.svg";
+import flexibleCommute from "./flexible-commute.svg";
+import freeVacation from "./free-vacation.svg";
+import latestEquipmentSupport from "./latest-equipment-support.svg";
+import mealSupport from "./meal-support.svg";
+import morningOff from "./morning-off.svg";
+import rewardForService from "./reward-for-service.svg";
+
+export default {
+  bookSupport,
+  culturalSupport,
+  flexibleCommute,
+  freeVacation,
+  latestEquipmentSupport,
+  mealSupport,
+  morningOff,
+  rewardForService,
+};
