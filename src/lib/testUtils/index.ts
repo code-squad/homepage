53555aa40fdd6f30d3b06d8a2f9015280dd97116
Angelo <valrentin.angelo@gmail.com>
2022-05-19 11:52:26 +0900

    :fire: strainMdxInfo와 중복되는 유틸 함수이므로 제거

--- src/lib/testUtils/index.ts
+++ src/lib/testUtils/index.ts
@@ -1,3 +1,2 @@
 export { default as TestProvider } from "./TestProvider";
-export { default as getQueryResultData } from "./getQueryResult";
 export { default as removeLineFeed } from "./removeLineFeed";
