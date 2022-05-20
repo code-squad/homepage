9f11629be929883deda4160fdc363e3686faf44d
HTMLhead <mrdoo309@gmail.com>
2022-05-20 14:42:16 +0900

    :bug: HomeGlobalNavigationBar => TeamGlobalNavigationBar로 수정

--- src/pages/team-culture/index.tsx
+++ src/pages/team-culture/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // Theme
 import GlobalTheme from "lib/context/GlobalTheme";
 // Components
-import { Footer, HomeGlobalNavigationBar } from "components";
+import { Footer, TeamGlobalNavigationBar } from "components";
 import {
   TeamIntroduce,
   TeamCulture,
@@ -15,7 +15,7 @@ const TeamCulturePage = () => {
   return (
     <GlobalTheme>
       <div style={{ overflowX: "hidden" }}>
-        <HomeGlobalNavigationBar />
+        <TeamGlobalNavigationBar />
         <TeamIntroduce />
         <TeamCulture />
         <TeamInterview />
