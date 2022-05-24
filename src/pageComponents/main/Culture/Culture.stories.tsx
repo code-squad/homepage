dbcb347f69cb4eaa33e15cc748ebe5036604d897
HTMLhead <mrdoo309@gmail.com>
2022-05-24 13:21:49 +0900

    :speech_balloon: culture컴포넌트에 대한 설명 mdx문서 제작

--- src/pageComponents/main/Culture/Culture.stories.tsx
+++ src/pageComponents/main/Culture/Culture.stories.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 // Story-Component
 import { Culture } from ".";
+import CultureDocs from "./Culture.docs.mdx";
 
 export default {
   title: "pagecomponent/main/Culture",
   component: Culture,
+  parameters: {
+    docs: {
+      page: CultureDocs,
+    },
+  },
 } as ComponentMeta<typeof Culture>;
 
 const Template: ComponentStory<typeof Culture> = () => <Culture />;
