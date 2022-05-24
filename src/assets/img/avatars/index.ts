e3a7767126f05b0a382c8cac557f7d15c6c409d3
HTMLhead <mrdoo309@gmail.com>
2022-05-24 18:35:06 +0900

    :bento: asset파일 이미지파일 정리하여 새로 추가

---
+++ src/assets/img/avatars/index.ts
@@ -0,0 +1,23 @@
+import profileChloe from "./profile-chloe.svg";
+import profileCrong from "./profile-Crong.svg";
+import profileHead from "./profile-Head.svg";
+import profileHonux from "./profile-Honux.svg";
+import profileIvy from "./profile-ivy.svg";
+import profileJk from "./profile-jk.svg";
+import profileMemberA from "./profile-member-a.svg";
+import profileMemberB from "./profile-member-b.svg";
+import profileSam from "./profile-sam.svg";
+import profileZello from "./profile-zello.svg";
+
+export default {
+  profileChloe,
+  profileCrong,
+  profileHead,
+  profileHonux,
+  profileIvy,
+  profileJk,
+  profileMemberA,
+  profileMemberB,
+  profileSam,
+  profileZello,
+};
---
+++ src/assets/img/icons/index.ts
@@ -0,0 +1,39 @@
+import book from "./book.svg";
+import calander from "./calander.svg";
+import chevronDown from "./chevron-down.svg";
+import chevronLeft from "./chevron-left.svg";
+import chevronRight from "./chevron-right.svg";
+import chevronUp from "./chevron-up.svg";
+import clock from "./clock.svg";
+import coin from "./coin.svg";
+import member from "./member.svg";
+import plus from "./plus.svg";
+import quotation from "./quotation.svg";
+import roundFilled from "./round-filled.svg";
+import roundLine from "./roundLine.svg";
+import codetogether from "./codetogether.svg";
+import masters from "./masters.svg";
+import pattern1 from "./pattern-1.svg";
+import pattern2 from "./pattern-2.svg";
+import pattern3 from "./pattern-3.svg";
+
+export default {
+  book,
+  calander,
+  chevronDown,
+  chevronLeft,
+  chevronRight,
+  chevronUp,
+  clock,
+  coin,
+  member,
+  plus,
+  quotation,
+  roundFilled,
+  roundLine,
+  codetogether,
+  masters,
+  pattern1,
+  pattern2,
+  pattern3,
+};
