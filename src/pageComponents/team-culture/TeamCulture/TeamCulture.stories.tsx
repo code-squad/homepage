f3d1912f0b38b42cfdc842688ddaac3cb0035898
Angelo <valrentin.angelo@gmail.com>
2022-05-24 15:52:57 +0900

    :memo: mdx작성 가이드라인 문서 적용

--- src/pageComponents/team-culture/TeamCulture/TeamCulture.stories.tsx
+++ src/pageComponents/team-culture/TeamCulture/TeamCulture.stories.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 // Story-Component
 import { TeamCulture } from ".";
+import TeamCultureDocs from "./TeamCulture.doc.mdx";
 
 export default {
   title: "pagecomponent/team-culture/TeamCulture",
   component: TeamCulture,
+  parameters: {
+    docs: {
+      page: TeamCultureDocs,
+    },
+  },
 } as ComponentMeta<typeof TeamCulture>;
 
 const Template: ComponentStory<typeof TeamCulture> = () => <TeamCulture />;
--- src/pageComponents/team-culture/TeamInterview/TeamInterview.stories.tsx
+++ src/pageComponents/team-culture/TeamInterview/TeamInterview.stories.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 // Story-Component
 import { TeamInterview } from ".";
+import TeamInterviewDocs from "./TeamInterview.doc.mdx";
 
 export default {
   title: "pagecomponent/team-culture/TeamInterview",
   component: TeamInterview,
+  parameters: {
+    docs: {
+      page: TeamInterviewDocs,
+    },
+  },
 } as ComponentMeta<typeof TeamInterview>;
 
 const Template: ComponentStory<typeof TeamInterview> = () => <TeamInterview />;
