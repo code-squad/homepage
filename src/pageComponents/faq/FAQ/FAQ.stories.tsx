8975b8e6b668cc3d4fa333d05d1b099480a1bb5f
HTMLhead <mrdoo309@gmail.com>
2022-05-24 15:14:06 +0900

    :speech_balloon: faq컴포넌트 문서 추가

--- src/pageComponents/faq/FAQ/FAQ.stories.tsx
+++ src/pageComponents/faq/FAQ/FAQ.stories.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 // Story-Component
 import { FAQ } from ".";
+import FAQDocs from "./FAQ.docs.mdx";
 
 export default {
   title: "pagecomponent/faq/FAQ",
   component: FAQ,
+  parameters: {
+    docs: {
+      page: FAQDocs,
+    },
+  },
 } as ComponentMeta<typeof FAQ>;
 
 const Template: ComponentStory<typeof FAQ> = () => <FAQ />;
