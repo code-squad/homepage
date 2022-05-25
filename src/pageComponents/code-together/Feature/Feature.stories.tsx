17ffb3236b26767680208d6675ff026550dc9e60
Angelo <valrentin.angelo@gmail.com>
2022-05-25 15:21:44 +0900

    :sparkles: 코드투게더 페이지의 Feature 컴포넌트 스토리 제작

---
+++ src/pageComponents/code-together/Feature/Feature.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+// Type
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+// Story-Component
+import { Feature } from ".";
+import FeatureDocs from "./Feature.docs.mdx";
+
+export default {
+  title: "pagecomponent/code-together/Feature",
+  component: Feature,
+  parameters: {
+    docs: {
+      page: FeatureDocs,
+    },
+  },
+} as ComponentMeta<typeof Feature>;
+
+const Template: ComponentStory<typeof Feature> = () => <Feature />;
+
+export const Default = Template.bind({});
