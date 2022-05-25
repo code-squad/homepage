00373777e6da69487ff78710f43873ebe5231605
Angelo <valrentin.angelo@gmail.com>
2022-05-25 15:05:50 +0900

    :white_check_mark: Course 컴포넌트의 테스트코드 제작

---
+++ src/pageComponents/code-together/Course/Course.test.mock.ts
@@ -0,0 +1,59 @@
+export const CourseQueryResult = {
+  mdx: {
+    frontmatter: {
+      courses: [
+        {
+          index: 1,
+          master: "크롱・ 프론트엔드",
+          title: "자바스크립트 Part.1",
+          dueDate: "5월 4일 오후 2시 마감",
+          price: "100,000원",
+          img: "jsPart1",
+          tags: ["입문", "초급"],
+        },
+        {
+          index: 2,
+          master: "크롱・ 프론트엔드",
+          title: "자바스크립트 Part.2",
+          dueDate: "5월 5일 오후 2시 마감",
+          price: "100,000원",
+          img: "jsPart2",
+          tags: ["입문", "초급"],
+        },
+        {
+          index: 3,
+          master: "크롱・ 프론트엔드",
+          title: "자바스크립트 Part.3",
+          dueDate: "5월 6일 오후 2시 마감",
+          price: "100,000원",
+          img: "jsPart3",
+          tags: ["입문", "초급"],
+        },
+      ],
+    },
+  },
+};
+
+const makeCourse = (index: number) => ({
+  index,
+  master: "크롱・ 프론트엔드",
+  title: `자바스크립트 Part.${index}`,
+  dueDate: `5월 ${index} 일 오후 2시 마감`,
+  price: "100,000원",
+  img: `jsPart${index}`,
+  tags: ["입문", "초급"],
+});
+const makeCourses = (count: number) => {
+  const courses = [];
+  for (let i = 0; i < count; i++) courses.push(makeCourse(i + 1));
+
+  return courses;
+};
+
+export const CourseQueryResult_ForMoreButton = {
+  mdx: {
+    frontmatter: {
+      courses: makeCourses(20),
+    },
+  },
+};
