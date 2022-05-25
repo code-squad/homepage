531197ac59eea513a439c92ac01ea3a695281bb6
Angelo <valrentin.angelo@gmail.com>
2022-05-25 16:24:31 +0900

    :white_check_mark: 클릭시 페이지 이동에 대한 테스트코드 추가

--- src/pageComponents/code-together/Masthead/Masthead.test.tsx
+++ src/pageComponents/code-together/Masthead/Masthead.test.tsx
@@ -37,4 +37,22 @@ describe("<Masthead>", () => {
       getByText(dueDate);
     });
   });
+  it("모집중인 과정의 이미지, 제목, 모집 마감일시가 보여진다.", () => {
+    const { getByText, getByAltText } = renderMasthead();
+
+    scheduledCourses.forEach(({ title, dueDate }: ScheduledCourse) => {
+      const featureImage = getByAltText(`course-img-${title}`);
+      expect(featureImage?.getAttribute("src")).toBe("test-file-stub");
+      getByText(title);
+      getByText(dueDate);
+    });
+  });
+  it("각 버튼을 클릭하면 path로 설정된곳으로 페이지가 이동된다.", () => {
+    const { getByLabelText } = renderMasthead();
+
+    scheduledCourses.forEach(({ title, path }: ScheduledCourse) => {
+      const courseCard = getByLabelText(`course-card-${title}`);
+      expect(courseCard.getAttribute("href")).toBe(path);
+    });
+  });
 });
