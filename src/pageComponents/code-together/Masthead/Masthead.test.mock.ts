1478ae30f4ad666c1290b6b1d66d9b6462b7288f
Angelo <valrentin.angelo@gmail.com>
2022-05-25 15:42:31 +0900

    :white_check_mark: 코드투게더 페이지의 Masthead 컴포넌트 테스트코드 제작

---
+++ src/pageComponents/code-together/Masthead/Masthead.test.mock.ts
@@ -0,0 +1,16 @@
+export const MastHeadQueryResult = {
+  mdx: {
+    frontmatter: {
+      title: "코드투게더",
+      description:
+        "코드투게더는 풀타임 수업 참여가 어려운 사람들을 대상으로, 다양한 수준의 과정들을 학습할 수 있는 코드스쿼드만의 교육 모델입니다. 다양한 미션들을 마주하며 프로그래밍에 대한 흥미를 키우고 개발 요구사항에 대응할 수 있는 능력을 기릅니다.",
+      scheduledCourses: [
+        { title: "자바스크립트 Part.1", dueDate: "조기마감", img: "jsPart1" },
+
+        { title: "자바스크립트 Part.2", dueDate: "5월 20일 오후 4시 마감", img: "jsPart2" },
+
+        { title: "자바스크립트 Part.3", dueDate: "5월 21일 오후 4시 마감", img: "jsPart3" },
+      ],
+    },
+  },
+};
---
+++ src/pageComponents/code-together/Masthead/Masthead.test.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import * as Gatsby from "gatsby";
+import { render } from "@testing-library/react";
+// Type
+import { ScheduledCourse } from "@type/ScheduledCourse";
+// Testing-Component
+import { Masthead } from ".";
+// Mock
+import { MastHeadQueryResult } from "./Masthead.test.mock";
+// Lib
+import { strainMdxInfo } from "lib/utils";
+import { TestProvider } from "lib/testUtils";
+
+describe("<Masthead>", () => {
+  const renderMasthead = () =>
+    render(
+      <TestProvider>
+        <Masthead />
+      </TestProvider>
+    );
+  const useStaticQuery = jest.spyOn(Gatsby, "useStaticQuery");
+  useStaticQuery.mockImplementation(() => MastHeadQueryResult);
+  const { title, description, scheduledCourses } = strainMdxInfo(MastHeadQueryResult);
+  it("제목과 설명이 보여진다.", () => {
+    const { getByText } = renderMasthead();
+
+    getByText(title);
+    getByText(description);
+  });
+  it("모집중인 과정의 이미지, 제목, 모집 마감일시가 보여진다.", () => {
+    const { getByText, getByAltText } = renderMasthead();
+
+    scheduledCourses.forEach(({ title, dueDate }: ScheduledCourse) => {
+      const featureImage = getByAltText(`course-img-${title}`);
+      expect(featureImage?.getAttribute("src")).toBe("test-file-stub");
+      getByText(title);
+      getByText(dueDate);
+    });
+  });
+});
