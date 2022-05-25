a11d3cc7dc51019604c0dff51c787fec419b7aa8
Angelo <valrentin.angelo@gmail.com>
2022-05-25 13:43:23 +0900

    :lipstick: 더 보기 버튼이 렌더링되도록 수정 및 더 보기 버튼 기능 구현

--- src/pageComponents/code-together/Course/Course.tsx
+++ src/pageComponents/code-together/Course/Course.tsx
@@ -3,14 +3,12 @@ import styled from "styled-components";
 import { graphql, useStaticQuery } from "gatsby";
 // Type
 import { CourseType } from "@type/Course";
-// Typography
-import { MBody } from "typography";
 // Components
-import { TitleSet } from "components";
+import { EButton, TitleSet } from "components";
 import { CourseCard } from "./CourseCard";
 // Assets
 import icons from "assets/img/icons";
-import { SUBTITLE, TITLE, DESCRIPTION } from "assets/static/phrases";
+import { SUBTITLE, TITLE } from "assets/static/phrases";
 // Utils
 import { strainMdxInfo } from "lib/utils";
 
@@ -18,17 +16,37 @@ const Course: React.FC = ({}) => {
   const data = useStaticQuery(CodeTogetherCourseQuery);
   const { courses }: { courses: CourseType[] } = strainMdxInfo(data);
 
+  const [courseCount, setCourseCount] = React.useState(courses.length > 9 ? 9 : courses.length);
+
+  const handleMoreButtonClick = () => {
+    if (courseCount + 9 <= courses.length) {
+      setCourseCount(courseCount + 5);
+      return;
+    }
+
+    setCourseCount(courses.length);
+  };
+
   return (
     <CourseWrapper>
       <TitleSet subtitle={SUBTITLE.CODE_TOGETHER_COURSE} title={TITLE.COURSE} />
       <CourseListWrapper>
         <CourseList>
-          {courses.map(({ master, title, dueDate, price, tags, img }) => (
+          {courses.slice(0, courseCount).map(({ master, title, dueDate, price, tags, img }) => (
             <li key={img}>
               <CourseCard {...{ master, title, dueDate, price, tags }} img={icons[img]} />
             </li>
           ))}
         </CourseList>
+        {courses.length > 9 && (
+          <MoreButtonWrapper>
+            <EButton
+              children="더보기"
+              disabled={courseCount === courses.length}
+              onClick={handleMoreButtonClick}
+            />
+          </MoreButtonWrapper>
+        )}
       </CourseListWrapper>
     </CourseWrapper>
   );
@@ -45,15 +63,31 @@ const CourseWrapper = styled.div`
 
 const CourseListWrapper = styled.div`
   width: 100%;
+  margin-top: 4rem;
+  padding: 8rem;
   background-color: ${({ theme: { color } }) => color.greyScale.offWhite};
 `;
 
 const CourseList = styled.ul`
-  padding: 8rem 18.9rem;
+  width: 106.2rem;
+  padding: 0 18.9rem;
   background-color: ${({ theme: { color } }) => color.greyScale.offWhite};
-  margin-top: 4rem;
   display: flex;
-  justify-content: space-between;
+  flex-flow: row wrap;
+  & > *:not(:nth-child(3n)) {
+    margin-right: 17.1rem;
+  }
+  & > *:not(:nth-last-child(-n + 3)) {
+    margin-bottom: 8rem;
+  }
+`;
+
+const MoreButtonWrapper = styled.div`
+  width: 100%;
+  margin-top: 8rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CodeTogetherCourseQuery = graphql`
