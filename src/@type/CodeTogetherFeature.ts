4169950f7ad5ca4fdc2f798384594f698bbcc272
Angelo <valrentin.angelo@gmail.com>
2022-05-26 19:00:02 +0900

    :coffin: 사용되지 않는 속성 관련 코드 제거

--- src/@type/CodeTogetherFeature.ts
+++ src/@type/CodeTogetherFeature.ts
@@ -1,7 +1,6 @@
 import illusts from "assets/img/illusts";
 
 export interface CodeTogetherFeatureType {
-  index?: number;
   title: string;
   img: keyof typeof illusts;
   descriptions: string[];
--- src/@type/Course.ts
+++ src/@type/Course.ts
@@ -1,7 +1,6 @@
 import icons from "assets/img/icons";
 
 export interface CourseType {
-  index?: number;
   master: string;
   title: string;
   dueDate: string;
