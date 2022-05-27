83f30ad5ffd09dbde7b82a8d7c47c274ef053f38
HTMLhead <mrdoo309@gmail.com>
2022-05-27 17:59:47 +0900

    :recycle: 반복되는 코드함수화

--- src/assets/img/image.stories.tsx
+++ src/assets/img/image.stories.tsx
@@ -1,78 +1,46 @@
 import React from "react";
+import styled from "styled-components";
 // Type
 import { Meta } from "@storybook/react";
+// Typography
+import { XSBody } from "typography";
 // Story-Image
 import avatars from "assets/img/avatars";
 import icons from "assets/img/icons";
 import illusts from "assets/img/illusts";
 import pictures from "assets/img/picture";
-import { XSBody } from "typography";
-import styled from "styled-components";
 
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
   title: "Home/assets/img",
 } as Meta;
 
-const avatarKeys = Object.keys(avatars) as Array<keyof typeof avatars>;
-const iconKeys = Object.keys(icons) as Array<keyof typeof icons>;
-const illustKeys = Object.keys(illusts) as Array<keyof typeof illusts>;
-const pictureKeys = Object.keys(pictures) as Array<keyof typeof picture>;
+const avatarKeys = Object.keys(avatars);
+const iconKeys = Object.keys(icons);
+const illustKeys = Object.keys(illusts);
+const pictureKeys = Object.keys(pictures);
 
-export const Avatars = () => (
-  <ImgWrapper>
-    {avatarKeys.map((avatar) => (
-      <div style={{ padding: "1rem" }}>
-        <div style={{ marginBottom: "1rem" }}>
-          <img style={{ border: "1px solid #000" }} src={avatars[avatar]} />
+const ImgList: React.FC<{ imgObj: any; imageKeys: string[] }> = ({ imgObj, imageKeys }) => {
+  return (
+    <ImgWrapper>
+      {imageKeys.map((img: string) => (
+        <div key={img} style={{ padding: "1rem" }}>
+          <div style={{ marginBottom: "1rem" }}>
+            <img style={{ border: "1px solid #000" }} src={imgObj[img]} />
+          </div>
+          <XSBody>{img}</XSBody>
         </div>
-        <XSBody>{avatar}</XSBody>
-      </div>
-    ))}
-  </ImgWrapper>
-);
+      ))}
+    </ImgWrapper>
+  );
+};
 
-export const Icons = () => (
-  <ImgWrapper>
-    {iconKeys.map((icon) => (
-      <div style={{ padding: "1rem" }}>
-        <div style={{ marginBottom: "1rem" }}>
-          <img style={{ border: "1px solid #000" }} src={icons[icon]} />
-        </div>
-        <XSBody>{icon}</XSBody>
-      </div>
-    ))}
-  </ImgWrapper>
-);
+export const Avatars = () => <ImgList imgObj={avatars} imageKeys={avatarKeys} />;
 
-export const Illusts = () => (
-  <ImgWrapper>
-    {illustKeys.map((illust) => (
-      <div style={{ padding: "1rem" }}>
-        <div style={{ marginBottom: "1rem" }}>
-          <img style={{ border: "1px solid #000" }} src={illusts[illust]} />
-        </div>
-        <XSBody>{illust}</XSBody>
-      </div>
-    ))}
-  </ImgWrapper>
-);
+export const Icons = () => <ImgList imgObj={icons} imageKeys={iconKeys} />;
 
-export const picture = () => (
-  <ImgWrapper>
-    {pictureKeys.map((picture) => (
-      <div style={{ padding: "1rem" }}>
-        <div style={{ marginBottom: "1rem" }}>
-          <img style={{ border: "1px solid #000" }} src={pictures[picture]} />
-        </div>
-        <XSBody>{picture}</XSBody>
-      </div>
-    ))}
-  </ImgWrapper>
-);
+export const Illusts = () => <ImgList imgObj={illusts} imageKeys={illustKeys} />;
+
+export const picture = () => <ImgList imgObj={pictures} imageKeys={pictureKeys} />;
 
 const ImgWrapper = styled.div`
   display: flex;
