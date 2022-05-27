3e4d114764d555e7f007ee893686c6c5bd1e3d89
Angelo <valrentin.angelo@gmail.com>
2022-05-27 18:47:26 +0900

    :coffin: 사용되지 않는 모듈 import문 제거

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { graphql } from "gatsby";
 // Type
 import { RegistrationType } from "@type/Registration";
 // Components
