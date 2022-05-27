99cc91e203bdf92f6d2e82a75a5e94cd8d5bdaa4
HTMLhead <mrdoo309@gmail.com>
2022-05-27 15:39:39 +0900

    :white_check_mark: 설명서 문서 추가 및 스토리컴포넌트 추가

--- src/pageComponents/main/Banner/Banner.stories.tsx
+++ src/pageComponents/main/Banner/Banner.stories.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 // Story-Component
 import { Banner } from ".";
+import BannerDocs from "./Banner.docs.mdx";
 
 export default {
   title: "pagecomponent/main/Banner",
   component: Banner,
+  parameters: {
+    docs: {
+      page: BannerDocs,
+    },
+  },
 } as ComponentMeta<typeof Banner>;
 
 const Template: ComponentStory<typeof Banner> = () => <Banner />;
