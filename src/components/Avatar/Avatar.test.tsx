10a5ce1ad52293bf22dfbbabc30b5b98ea4f5d1c
Angelo <valrentin.angelo@gmail.com>
2022-05-30 19:38:33 +0900

    :white_check_mark: 변경된 상대경로를 참조하도록 수정

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -7,7 +7,7 @@ import { TestProvider } from "lib/testUtils";
 
 describe("<Avatar>", () => {
   it("아바타 컴포넌트는 props로 들어온 src의 주소를 가진 이미지가 보여진다.", async () => {
-    const imgSrc = "assets/img/avatars/profile-member-a.svg";
+    const imgSrc = "assets/img/avatars/80-member1.svg";
     const { findByAltText } = render(
       <TestProvider>
         <Avatar src={imgSrc} />
