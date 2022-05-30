3151ae362dadb4af39d311a5503fa6ff99b0b111
Angelo <valrentin.angelo@gmail.com>
2022-05-30 19:39:41 +0900

    :white_check_mark: disabled상태일경우 버튼이 클릭되지 않는 사양에대한 테스트코드 추가

--- src/components/EButton/EButton.test.tsx
+++ src/components/EButton/EButton.test.tsx
@@ -11,22 +11,31 @@ describe("<EButton>", () => {
   const onClick = jest.fn();
   const props = {
     accent: false,
-    disabled: false,
     onClick,
   };
-  const renderEButton = (buttonText: string) =>
+  const renderEButton = (disabled: boolean = false) =>
     render(
       <TestProvider>
-        <EButton {...props}>{buttonText}</EButton>
+        <EButton {...props} {...{ disabled }}>
+          {LINK.SUBSCRIPTION}
+        </EButton>
       </TestProvider>
     );
   it("버튼 문구가 보여진다.", () => {
-    const { getByText } = renderEButton(LINK.SUBSCRIPTION);
+    const { getByText } = renderEButton();
 
     getByText(LINK.SUBSCRIPTION);
   });
+  it("disabled상태일경우 버튼을 클릭해도 props로 전달된 onClick 콜백이 호출되지 않는다.", () => {
+    const { getByText } = renderEButton(true);
+
+    const button = getByText(LINK.SUBSCRIPTION);
+    fireEvent.click(button);
+
+    expect(onClick).toHaveBeenCalledTimes(0);
+  });
   it("버튼을 클릭하면 props로 전달된 onClick 콜백이 호출된다.", () => {
-    const { getByText } = renderEButton(LINK.SUBSCRIPTION);
+    const { getByText } = renderEButton();
 
     const button = getByText(LINK.SUBSCRIPTION);
     fireEvent.click(button);
