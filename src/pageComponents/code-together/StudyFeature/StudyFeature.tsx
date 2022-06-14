f567267fd565ba7dd64c973d290c1d9858807773
Angelo <valrentin.angelo@gmail.com>
2022-06-14 18:41:38 +0900

    :lipstick: 화살표 및 어떻게 학습하나요 이미지가 보여지지 않도록 주석처리

--- src/pageComponents/code-together/StudyFeature/StudyFeature.tsx
+++ src/pageComponents/code-together/StudyFeature/StudyFeature.tsx
@@ -7,10 +7,10 @@ import { CodeTogetherFeatureType } from "@type/CodeTogetherFeature";
 import { TitleSet } from "components";
 import { ImageCard } from "./ImageCard";
 // Assets
-import icons from "assets/img/icons";
+// import icons from "assets/img/icons";
+// import features from "assets/img/illusts/feature";
 import features from "assets/img/illusts/feature";
-import illusts from "assets/img/illusts";
-import picture from "assets/img/picture";
+// import picture from "assets/img/picture";
 import { SUBTITLE, TITLE } from "assets/static/phrases";
 // Utils
 import { strainMdxInfo } from "lib/utils";
@@ -19,21 +19,22 @@ const StudyFeature: React.FC = () => {
   const data = useStaticQuery(CodeTogetherStudyFeatureQuery);
   const { studyFeatures }: { studyFeatures: CodeTogetherFeatureType[] } = strainMdxInfo(data);
 
-  const { placeBlank } = illusts;
-  const imgList = [picture.howToStudy, placeBlank];
-  const [currentIndex, setCurrentIndex] = React.useState(0);
+  // const { placeBlank } = illusts;
+  // const imgList = [picture.howToStudy, placeBlank];
+  // const [currentIndex, setCurrentIndex] = React.useState(0);
 
-  const handleArrowLeftClick = () => {
-    if (currentIndex - 1 >= 0) setCurrentIndex(currentIndex - 1);
-  };
+  // const handleArrowLeftClick = () => {
+  //   if (currentIndex - 1 >= 0) setCurrentIndex(currentIndex - 1);
+  // };
 
-  const handleArrowRightClick = () => {
-    if (currentIndex + 1 <= imgList.length) setCurrentIndex(currentIndex + 1);
-  };
+  // const handleArrowRightClick = () => {
+  //   if (currentIndex + 1 <= imgList.length) setCurrentIndex(currentIndex + 1);
+  // };
 
   return (
     <StudyWrapper>
-      <ArrowNavigationWrapper>
+      <TitleSet subtitle={SUBTITLE.CODE_TOGETHER} title={TITLE.HOW_STUDY} />
+      {/* <ArrowNavigationWrapper>
         <ArrowButton disabled={currentIndex === 0} onClick={handleArrowLeftClick}>
           <img src={icons.chevronLeft} alt="arrow-left" />
         </ArrowButton>
@@ -41,21 +42,19 @@ const StudyFeature: React.FC = () => {
           <img src={icons.chevronRight} alt="arrow-right" />
         </ArrowButton>
       </ArrowNavigationWrapper>
-      <TitleSet subtitle={SUBTITLE.CODE_TOGETHER} title={TITLE.HOW_STUDY} />
       <StudyFeatureImgListWrapper>
         <StudyFeatureImgList {...{ currentIndex }}>
           {imgList.map((image, i) => (
-            // 이미지 추가 후 이미지 수정 필요
             <li key={`${image}-${i}`}>
               <StudyFeatureImg src={image} alt="codetogether-study-feature" />
             </li>
           ))}
         </StudyFeatureImgList>
-      </StudyFeatureImgListWrapper>
+      </StudyFeatureImgListWrapper> */}
       <FeatureList>
         {studyFeatures.map(({ title, descriptions, img }) => (
           <FeatureItem key={title}>
-            <ImageCard {...{ title, descriptions }} img={illusts.illustCardBlank} medium />
+            <ImageCard {...{ title, descriptions }} img={features[img]} medium />
           </FeatureItem>
         ))}
       </FeatureList>
@@ -77,52 +76,52 @@ const StudyWrapper = styled.div`
   }
 `;
 
-const ArrowNavigationWrapper = styled.div`
-  width: 7.6rem;
-  height: 3rem;
-  position: absolute;
-  margin-top: 6.4rem;
-  margin-left: 98.6rem;
-  display: flex;
-  & > *:not(:last-child) {
-    margin-right: 1.6rem;
-  }
-`;
+// const ArrowNavigationWrapper = styled.div`
+//   width: 7.6rem;
+//   height: 3rem;
+//   position: absolute;
+//   margin-top: 6.4rem;
+//   margin-left: 98.6rem;
+//   display: flex;
+//   & > *:not(:last-child) {
+//     margin-right: 1.6rem;
+//   }
+// `;
 
-const ArrowButton = styled.button`
-  width: 3rem;
-  height: 3rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: transparent;
-  border: 0;
-  &:hover {
-    cursor: pointer;
-  }
-  &:disabled {
-    cursor: auto;
-    filter: invert(50%) sepia(7%) saturate(6%) hue-rotate(351deg) brightness(92%) contrast(89%);
-  }
-`;
+// const ArrowButton = styled.button`
+//   width: 3rem;
+//   height: 3rem;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: transparent;
+//   border: 0;
+//   &:hover {
+//     cursor: pointer;
+//   }
+//   &:disabled {
+//     cursor: auto;
+//     filter: invert(50%) sepia(7%) saturate(6%) hue-rotate(351deg) brightness(92%) contrast(89%);
+//   }
+// `;
 
-const StudyFeatureImgListWrapper = styled.div`
-  width: 77.8rem;
-  margin: 0 auto;
-  overflow: hidden;
-`;
+// const StudyFeatureImgListWrapper = styled.div`
+//   width: 77.8rem;
+//   margin: 0 auto;
+//   overflow: hidden;
+// `;
 
-const StudyFeatureImgList = styled.ul<{ currentIndex: number }>`
-  display: flex;
-  position: relative;
-  display: flex;
-  transition: left 0.5s;
-  left: -${({ currentIndex }) => currentIndex * 77.8}rem;
-`;
+// const StudyFeatureImgList = styled.ul<{ currentIndex: number }>`
+//   display: flex;
+//   position: relative;
+//   display: flex;
+//   transition: left 0.5s;
+//   left: -${({ currentIndex }) => currentIndex * 77.8}rem;
+// `;
 
-const StudyFeatureImg = styled.img`
-  width: 77.8rem;
-`;
+// const StudyFeatureImg = styled.img`
+//   width: 77.8rem;
+// `;
 
 const FeatureList = styled.ul`
   width: 106.2rem;
