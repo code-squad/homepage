1b1a6382b0bb5a1e4ac2e884da9c88a551360aac
Angelo <valrentin.angelo@gmail.com>
2022-06-15 11:47:38 +0900

    :white_check_mark: 업데이트된 팀 문화 컨텐츠 적용

--- src/pageComponents/team-culture/TeamCulture/TeamCulture.test.mock.ts
+++ src/pageComponents/team-culture/TeamCulture/TeamCulture.test.mock.ts
@@ -6,14 +6,14 @@ export const TeamCultureQueryResult = {
           title: "자율과 책임",
           cultureFeatures: [
             {
-              subtitle: "자율 업무 환경",
+              subtitle: "자율적인 업무 환경",
               description:
-                "학생들에게 더 나은 배움터를 제공한다는 비전을 중심으로 자신의 업무를 선택하고 판단해요.",
+                "학습자에게 더 나은 배움터를 제공한다는 비전을 중심으로 자신의 역할과 범위를 스스로 판단하고 선택합니다.",
             },
             {
               subtitle: "성숙한 구성원",
               description:
-                "팀원들은 자신이 하는 선택이 팀에 어떤 영향을 미치는지 충분히 고려하여 선택하고 그 책임을 감당할 수 있어야 해요.",
+                "자신의 선택이 팀에 어떤 영향을 미칠지 충분히 고려하고, 그 선택에 따른 책임을 감당합니다.",
             },
           ],
         },
@@ -21,13 +21,14 @@ export const TeamCultureQueryResult = {
           title: "존중과 포용",
           cultureFeatures: [
             {
-              subtitle: "위계 없는 수평 구조",
+              subtitle: "위계없이 수평적으로",
               description:
-                "직급이 없이 서로 존중하며 자유롭게 소통할 수 있도록 닉네임과 존대를 함께 사용해요.",
+                "직급없이 닉네임을 사용하고, 서로를 존중하고 지지하며 자유롭게 소통합니다.",
             },
             {
-              subtitle: "타이틀1",
-              description: "내용1",
+              subtitle: "서로에 대한 이해",
+              description:
+                "개개인의 다양성을 이해하고, 서로의 의견을 경청하며 각자의 방식으로 포용합니다.",
             },
           ],
         },
