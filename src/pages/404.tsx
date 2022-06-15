637d92f86f7c213e776457b3a0921a5a50f481d6
Angelo <valrentin.angelo@gmail.com>
2022-06-15 12:43:20 +0900

    :lipstick: GNB/Footer 및 NotFound 컴포넌트를 렌더링하도록 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,54 +1,20 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
+// Theme
+import GlobalTheme from "lib/context/GlobalTheme";
+// Components
+import { Footer, HomeGlobalNavigationBar } from "components";
+import { NotFound } from "pageComponents/notFound";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+const NotFoundPage: React.FC = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <GlobalTheme>
+      <main style={{ overflowX: "hidden" }}>
+        <HomeGlobalNavigationBar />
+        <NotFound />
+        <Footer />
+      </main>
+    </GlobalTheme>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
