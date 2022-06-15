acc6e4279ec160fc1d9c4e0915d41ecd47409553
HTMLhead <mrdoo309@gmail.com>
2022-06-15 12:05:08 +0900

    :sparkles: jobposition 코드 리팩토링 및 테스트 추가
    allMdx에 맞는 목 추가 및 테스트 수정, strainAllMdxInfoBody import구문 수정

--- src/pageComponents/recruit/JobPosition/JobPosition.test.tsx
+++ src/pageComponents/recruit/JobPosition/JobPosition.test.tsx
@@ -11,7 +11,7 @@ import { JobPositionResult } from "./JobPosition.test.mock";
 import { TITLE } from "assets/static/phrases";
 // Libs
 import { TestProvider, removeLineFeed } from "lib/testUtils";
-import { strainMdxInfo } from "lib/utils";
+import { strainAllMdxInfoBody } from "lib/utils";
 
 describe("<JobPosition>", () => {
   const renderJobPosition = () =>
@@ -29,14 +29,13 @@ describe("<JobPosition>", () => {
   });
   it("채용 분야, 포지션, 채용 정보, 최종 업데이트 날짜가 보여진다.", async () => {
     const { getByText, getAllByLabelText } = renderJobPosition();
-    const { jobPositions } = strainMdxInfo(JobPositionResult);
+    const jobPositionList = strainAllMdxInfoBody(JobPositionResult);
 
     const arrowDownImages = getAllByLabelText("arrow-down");
-    expect(arrowDownImages.length).toEqual(jobPositions.length);
+    expect(arrowDownImages.length).toEqual(jobPositionList.length);
 
-    jobPositions.forEach(({ title, content, editDate }: JobPositionType) => {
+    jobPositionList.forEach(({ title, editDate }: JobPositionType) => {
       getByText(title);
-      getByText(removeLineFeed(content));
       getByText(`최종 업데이트: ${editDate}`);
     });
   });
