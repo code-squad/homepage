0ba67a8544169b823dc93db6d9e526ebb6999234
Angelo <valrentin.angelo@gmail.com>
2022-06-16 11:24:19 +0900

    :memo: StudyFeauture 스토리 제작 및 StudyFeature 문서 적용

--- src/pageComponents/code-together/StudyFeature/StudyFeature.stories.tsx
+++ src/pageComponents/code-together/StudyFeature/StudyFeature.stories.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 // Story-Component
 import { StudyFeature } from ".";
+import StudyFeatureDocs from "./StudyFeature.docs.mdx";
 
 export default {
   title: "pagecomponent/code-together/StudyFeature",
   component: StudyFeature,
+  parameters: {
+    docs: {
+      page: StudyFeatureDocs,
+    },
+  },
 } as ComponentMeta<typeof StudyFeature>;
 
 const Template: ComponentStory<typeof StudyFeature> = () => <StudyFeature />;
