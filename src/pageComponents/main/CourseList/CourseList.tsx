b3d7fe2cb31cc30dbe09912e040074db7d14d6ba
Angelo <valrentin.angelo@gmail.com>
2022-08-17 14:28:21 +0900

    :lipstick: 신규 타이틀 적용, 컴포넌트 배치 변경

--- src/pageComponents/main/CourseList/CourseList.tsx
+++ src/pageComponents/main/CourseList/CourseList.tsx
@@ -1,35 +1,47 @@
 import React from "react";
 import styled from "styled-components";
 // Components
-import { LinkButton } from "components";
+import { LinkButton, TitleSet } from "components";
 // Assets
 import thumbnails from "assets/img/illusts/thumbnail";
-import { LINK_DESCRIPTION, LINK } from "assets/static/phrases";
+import { TITLE, LINK_DESCRIPTION, LINK } from "assets/static/phrases";
 import { INTERNAL } from "assets/static/urls";
 
 const CourseList: React.FC = () => {
   return (
     <CourseWrapper>
-      <LinkButton
-        to={INTERNAL.MASTERS}
-        title={LINK.MASTERS}
-        description={LINK_DESCRIPTION.MASTERS}
-        icon={thumbnails.mediumMastersCourse}
-      />
-      <LinkButton
-        to={INTERNAL.CODE_TOGETHER}
-        title={LINK.CODE_TOGETHER}
-        description={LINK_DESCRIPTION.CODE_TOGETHER}
-        icon={thumbnails.mediumCodeTogether}
-      />
+      <TitleSet subtitle={TITLE.VIEW_COURSES}></TitleSet>
+      <CourseListWrapper>
+        <LinkButton
+          to={INTERNAL.MASTERS}
+          title={LINK.MASTERS}
+          description={LINK_DESCRIPTION.MASTERS}
+          icon={thumbnails.mediumMastersCourse}
+        />
+        <LinkButton
+          to={INTERNAL.CODE_TOGETHER}
+          title={LINK.CODE_TOGETHER}
+          description={LINK_DESCRIPTION.CODE_TOGETHER}
+          icon={thumbnails.mediumCodeTogether}
+        />
+      </CourseListWrapper>
     </CourseWrapper>
   );
 };
 
 const CourseWrapper = styled.ul`
-  min-width: 144rem;
+  width: 106.2rem;
+  padding: 0 18.9rem;
   margin-top: 8rem;
-  margin-bottom: 15rem;
+  margin-bottom: 18rem;
+  display: flex;
+  flex-direction: column;
+  & > *:not(:last-child) {
+    margin-bottom: 3.2rem;
+  }
+`;
+
+const CourseListWrapper = styled.ul`
   display: flex;
   justify-content: center;
   & > *:not(:last-child) {
