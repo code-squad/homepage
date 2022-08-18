e1aeb9bc15dc709e3af1e6614d1bdb8f43580256
Angelo <valrentin.angelo@gmail.com>
2022-08-18 16:54:02 +0900

    :bento: 클린 스위프트 코스 이미지 추가

--- src/assets/img/illusts/thumbnail/index.ts
+++ src/assets/img/illusts/thumbnail/index.ts
@@ -1,15 +1,19 @@
 import mediumCodeTogether from "./medium-ct.svg";
 import mediumMastersCourse from "./medium-mc.svg";
 import mediumJavascript from "./medium-js.svg";
+import mediumSwift from "./medium-sw.svg";
 import smallCodeTogether from "./small-ct.svg";
 import smallMastersCourse from "./small-mc.svg";
 import smallJavascript from "./small-js.svg";
+import smallSwift from "./small-sw.svg";
 
 export default {
   mediumCodeTogether,
   mediumMastersCourse,
   mediumJavascript,
+  mediumSwift,
   smallCodeTogether,
   smallMastersCourse,
   smallJavascript,
+  smallSwift,
 };
