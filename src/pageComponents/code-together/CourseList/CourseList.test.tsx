132c7e6aa6d45bc3845be1f4b0f88880fd79894d
Angelo <valrentin.angelo@gmail.com>
2022-08-19 17:24:22 +0900

    :white_check_mark: 코드투게더 페이지에서 보여지는 모집중 과정에 대한 테스트 코드로 수정

--- src/pageComponents/code-together/CourseList/CourseList.test.tsx
+++ src/pageComponents/code-together/CourseList/CourseList.test.tsx
@@ -4,7 +4,7 @@ import { render } from "@testing-library/react";
 import { CourseList } from ".";
 // Assets
 import { LINK, LINK_DESCRIPTION } from "assets/static/phrases";
-import { INTERNAL } from "assets/static/urls";
+import { EXTERNAL, INTERNAL } from "assets/static/urls";
 // Libs
 import { TestProvider } from "lib/testUtils";
 
@@ -15,20 +15,20 @@ describe("<CourseList>", () => {
         <CourseList />
       </TestProvider>
     );
-  it("마스터즈 코스 링크가 보여진다.", async () => {
+  it("자바스크립트 코스 링크가 보여진다.", async () => {
     const { getByText, getAllByRole } = renderCourseList();
-    getByText(LINK.MASTERS);
-    getByText(LINK_DESCRIPTION.MASTERS);
+    getByText(LINK.JAVASCRIPT);
+    getByText(LINK_DESCRIPTION.JAVASCRIPT);
 
-    const [mastersLink] = getAllByRole("link");
-    expect(mastersLink?.getAttribute("href")).toBe(INTERNAL.MASTERS);
+    const [javascriptLink] = getAllByRole("link");
+    expect(javascriptLink?.getAttribute("href")).toBe(INTERNAL.JAVASCRIPT);
   });
-  it("코드투게더 링크가 보여진다..", async () => {
+  it("코드투게더 링크가 보여진다.", async () => {
     const { getByText, getAllByRole } = renderCourseList();
-    getByText(LINK.CODE_TOGETHER);
-    getByText(LINK_DESCRIPTION.CODE_TOGETHER);
+    getByText(LINK.CLEAN_SWIFT);
+    getByText(LINK_DESCRIPTION.CLEAN_SWIFT);
 
     const [_, codeTogetherLink] = getAllByRole("link");
-    expect(codeTogetherLink?.getAttribute("href")).toBe(INTERNAL.CODE_TOGETHER);
+    expect(codeTogetherLink?.getAttribute("href")).toBe(EXTERNAL.CLEAN_SWIFT);
   });
 });
