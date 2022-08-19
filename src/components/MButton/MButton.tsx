27cb89dc8caa5d515d98aba263bf44ecda97b966
Angelo <valrentin.angelo@gmail.com>
2022-08-19 16:34:14 +0900

    :lipstick: url로 전달된 to가 외부 페이지일 경우 새 탭으로 보여지도록 수정, accent 컬러값 추가

--- src/components/MButton/MButton.tsx
+++ src/components/MButton/MButton.tsx
@@ -14,16 +14,21 @@ export interface IMButtonProps {
 }
 
 const MButton: React.FC<IMButtonProps> = ({ children, accent, disabled, to }) => {
+  const urlRegex = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
   return (
     <MButtonWrapper
       $accent={accent}
       $disabled={disabled}
       href={to}
-      target="_blank"
-      rel="noopener noreferrer nofollow"
+      target={to.match(urlRegex) ? "_blank" : undefined}
+      rel={to.match(urlRegex) ? "noopener noreferrer nofollow" : undefined}
     >
       <MBold>{children}</MBold>
-      <ButtonIcon src={icons.chevronRight} {...{ disabled }} />
+      <ButtonIcon
+        src={icons.chevronRight}
+        {...{ accent, disabled }}
+        style={{ marginLeft: "0.4rem" }}
+      />
     </MButtonWrapper>
   );
 };
@@ -32,43 +37,61 @@ const MButtonWrapper = styled.a<{ $accent?: boolean; $disabled?: boolean }>`
   display: inline-flex;
   justify-content: space-between;
   align-items: center;
-  height: 4rem;
+  height: 3.8rem;
   padding: 0 2.4rem;
-  color: ${({ $disabled, theme: { color } }) =>
-    $disabled ? color.greyScale.grey3 : color.greyScale.grey1};
+  color: ${({ $accent, $disabled, theme: { color } }) =>
+    $disabled
+      ? color.greyScale.grey4
+      : $accent
+      ? color.blackAndWhite.white
+      : color.greyScale.grey1};
   border: 0.1rem solid;
   border-radius: 3rem;
   border-color: ${({ $accent, $disabled, theme: { color } }) =>
     $disabled
-      ? color.greyScale.grey3
+      ? color.greyScale.grey4
       : $accent
       ? color.blackAndWhite.black
       : color.greyScale.grey3};
-  background-color: ${({ theme: { color } }) => color.blackAndWhite.white};
+  background-color: ${({ $disabled, $accent, theme: { color } }) =>
+    $disabled
+      ? color.blackAndWhite.white
+      : $accent
+      ? color.blackAndWhite.black
+      : color.blackAndWhite.white};
   text-decoration: none;
   text-align: center;
   cursor: ${({ $disabled }) => ($disabled ? "unset" : "pointer")};
   pointer-events: ${({ $disabled }) => ($disabled ? "none" : "auto")};
   &:hover {
     color: ${({ $disabled, theme: { color } }) =>
-      $disabled ? color.greyScale.grey3 : color.blackAndWhite.black};
+      $disabled ? color.greyScale.grey4 : color.blackAndWhite.black};
     background-color: ${({ $disabled, theme: { color } }) =>
       $disabled ? color.blackAndWhite.white : color.primary.green4};
     border-color: ${({ $disabled, theme: { color } }) =>
-      $disabled ? color.greyScale.grey3 : color.blackAndWhite.black};
+      $disabled ? color.greyScale.grey4 : color.blackAndWhite.black};
   }
   &:disabled {
     cursor: auto;
   }
 `;
 
-const ButtonIcon = styled.img<{ disabled?: boolean }>`
+const ButtonIcon = styled.img<{ accent?: boolean; disabled?: boolean }>`
   width: 1.6rem;
   height: 1.6rem;
-  filter: ${({ disabled }) =>
+  filter: ${({ accent, disabled }) =>
     disabled
-      ? "invert(79%) sepia(29%) saturate(7%) hue-rotate(104deg) brightness(94%) contrast(91%)"
+      ? "invert(87%) sepia(1%) saturate(45%) hue-rotate(314deg) brightness(105%) contrast(85%)"
+      : accent
+      ? "invert(99%) sepia(99%) saturate(0%) hue-rotate(346deg) brightness(107%) contrast(101%)"
       : "none"};
+  ${MButtonWrapper}:hover & {
+    filter: ${({ accent, disabled }) =>
+      disabled
+        ? "invert(85%) sepia(0%) saturate(0%) hue-rotate(69deg) brightness(100%) contrast(100%)"
+        : accent
+        ? "none"
+        : "invert(8%) sepia(4%) saturate(11%) hue-rotate(79deg) brightness(104%) contrast(94%)"}
 `;
 
 export default MButton;
