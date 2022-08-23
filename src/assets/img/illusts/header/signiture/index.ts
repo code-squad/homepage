ca387b7975ef0955cd5f784f51286f4acc9a04da
Angelo <valrentin.angelo@gmail.com>
2022-08-23 16:46:36 +0900

    :fire: 사용되지않는 리소스 제거

--- src/assets/img/illusts/header/signiture/index.ts
+++ src/assets/img/illusts/header/signiture/index.ts
@@ -1,11 +1,7 @@
 import home1 from "./home1.svg";
 import home2 from "./home2.svg";
-import team1 from "./team1.svg";
-import team2 from "./team2.svg";
 
 export default {
   home1,
   home2,
-  team1,
-  team2,
 };
