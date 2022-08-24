49fbc1a70eeae8ae3231f717e03cd0cbf6482a61
Angelo <valrentin.angelo@gmail.com>
2022-08-24 18:23:17 +0900

    :lipstick: 반응형 모바일 UI 적용

--- src/pageComponents/main/Culture/Culture.tsx
+++ src/pageComponents/main/Culture/Culture.tsx
@@ -4,29 +4,50 @@ import { graphql, useStaticQuery } from "gatsby";
 // Type
 import { CultureType } from "@type/Culture";
 // Typography
-import { HLBold, MBold, MBody } from "typography";
+import { HLBold, SHLBold, MBold, MBody } from "typography";
 // Components
-import { TitleSet } from "components/";
+import { RectangleNavigation, TitleSet } from "components/";
 // Assets
 import features from "assets/img/illusts/feature";
 import { SUBTITLE, TITLE } from "assets/static/phrases";
 // Lib
 import { strainMdxInfo, getSplittedPhrase } from "lib/utils";
+import { useResponsive } from "lib/hooks";
 
 const Culture: React.FC = () => {
+  const { isMobile } = useResponsive();
+
   const { cultures }: { cultures: CultureType[] } = strainMdxInfo(useStaticQuery(CultureQuery));
+
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  const handleIndexChanged = (index: number) => {
+    setCurrentIndex(index);
+  };
 
   return (
     <CultureWrapper>
-      <TitleSet subtitle={SUBTITLE.CULTURE} title={TITLE.CULTURE} />
-      <ContentWrapper>
+      <TitleWrapper>
+        <TitleSet subtitle={SUBTITLE.CULTURE} title={TITLE.CULTURE} />
+      </TitleWrapper>
+      <ContentWrapper {...{ currentIndex }}>
         {cultures.map(({ title, image, subtitle, description }) => (
           <CultureContent key={title}>
             <CultureImg src={features[image]} alt="culture-icon" />
-            <TitleWrapper>
-              <MBold>{subtitle}</MBold>
-              <HLBold>{title}</HLBold>
-            </TitleWrapper>
+            <CultureTitleWrapper>
+              {isMobile && (
+                <>
+                  <MBold>{subtitle}</MBold>
+                  <SHLBold>{title}</SHLBold>
+                </>
+              )}
+              {!isMobile && (
+                <>
+                  <MBold>{subtitle}</MBold>
+                  <HLBold>{title}</HLBold>
+                </>
+              )}
+            </CultureTitleWrapper>
             <DescriptionList>
               {getSplittedPhrase(description).map((descriptionItem: string) => (
                 <DescriptionItem key={descriptionItem}>
@@ -37,62 +58,127 @@ const Culture: React.FC = () => {
           </CultureContent>
         ))}
       </ContentWrapper>
+      {isMobile && (
+        <RectangleNavigationWrapper>
+          <RectangleNavigation count={3} index={currentIndex} onIndexChanged={handleIndexChanged} />
+        </RectangleNavigationWrapper>
+      )}
     </CultureWrapper>
   );
 };
 
 const CultureWrapper = styled.div`
-  width: 106.2rem;
-  padding: 0 18.9rem;
-  padding-bottom: 8rem;
-  margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: space-between;
-  & > *:not(:last-child) {
-    margin-bottom: 5.6rem;
+  @media (min-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    width: 100%;
+    & > *:not(:last-child) {
+      margin-bottom: 2.4rem;
+    }
+  }
+  @media (min-width: ${({ theme }) => theme.breakPoint.desktop}) {
+    width: 106.2rem;
+    padding: 0 18.9rem;
+    padding-bottom: 8rem;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: space-between;
+    & > *:not(:last-child) {
+      margin-bottom: 5.6rem;
+    }
   }
 `;
 
-const ContentWrapper = styled.div`
+const TitleWrapper = styled.div`
+  @media (min-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    padding: 0 2.4rem;
+  }
+  @media (min-width: ${({ theme }) => theme.breakPoint.desktop}) {
+    padding: 0;
+  }
+`;
+
+const ContentWrapper = styled.ul<{ currentIndex: number }>`
+  position: relative;
   display: flex;
-  white-space: pre-line;
-  & > *:not(:last-child) {
-    margin-right: 7.8rem;
+  @media (min-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    width: 300%;
+    flex-direction: row;
   }
+  @media (min-width: ${({ theme }) => theme.breakPoint.desktop}) {
+    flex-direction: row;
+    white-space: pre-line;
+    & > *:not(:last-child) {
+      margin-right: 7.8rem;
+    }
+  }
+  transition: left 0.5s;
+  left: -${({ currentIndex }) => currentIndex * 100}%;
 `;
 
-const CultureContent = styled.div`
-  width: 30.2rem;
+const CultureContent = styled.li`
   display: flex;
   flex-direction: column;
   & > *:not(:last-child) {
     margin-bottom: 2.4rem;
   }
+  @media (min-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    width: 100%;
+  }
+  @media (min-width: ${({ theme }) => theme.breakPoint.desktop}) {
+    width: 30.2rem;
+  }
 `;
 
 const CultureImg = styled.img`
-  width: 8rem;
-  height: 8rem;
+  @media (min-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    width: 100%;
+    height: 21rem;
+    background-color: ${({ theme: { color } }) => color.surface.offWhite1};
+  }
+  @media (min-width: ${({ theme }) => theme.breakPoint.desktop}) {
+    width: 8rem;
+    height: 8rem;
+    background-color: transparent;
+  }
 `;
 
-const TitleWrapper = styled.div`
+const CultureTitleWrapper = styled.div`
   color: ${({ theme: { color } }) => color.blackAndWhite.black};
   & > *:not(:last-child) {
-    padding-bottom: 0.8rem;
+    padding-bottom: 0.4rem;
+  }
+  @media (min-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    padding: 0 2.4rem;
+  }
+  @media (min-width: ${({ theme }) => theme.breakPoint.desktop}) {
+    padding: 0;
   }
 `;
 
 const DescriptionList = styled.ul`
   display: flex;
   flex-direction: column;
-  & > *:not(:last-child) {
-    margin-bottom: 0.8rem;
+  @media (min-width: ${({ theme }) => theme.breakPoint.mobile}) {
+    padding: 0 2.4rem;
+  }
+  @media (min-width: ${({ theme }) => theme.breakPoint.desktop}) {
+    padding: 0;
+    & > *:not(:last-child) {
+      margin-bottom: 0.8rem;
+    }
   }
 `;
 
 const DescriptionItem = styled.li`
   color: ${({ theme: { color } }) => color.greyScale.grey2};
+`;
+
+const RectangleNavigationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 const CultureQuery = graphql`
