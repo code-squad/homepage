7596dc23581848f7667690c7319825ba606e5dc2
Angelo <valrentin.angelo@gmail.com>
2022-08-25 11:52:09 +0900

    :hammer: device theme을 사용하여 장치 여부를 반환하도록 수정

--- src/lib/hooks/useResponsive.ts
+++ src/lib/hooks/useResponsive.ts
@@ -1,14 +1,13 @@
-import React from "react";
 import { useTheme } from "styled-components";
 import { useMediaQuery } from "react-responsive";
 
 const useScrollPosition = () => {
   const {
-    breakPoint: { tablet, desktop },
+    device: { mobile, tablet, desktop },
   } = useTheme();
-  const isMobile = useMediaQuery({ query: `(max-width: calc(${tablet} - 1px))` });
-  const isTablet = useMediaQuery({ query: `(min-width: calc(${tablet})` });
-  const isDesktop = useMediaQuery({ query: `(min-width: ${desktop}` });
+  const isMobile = useMediaQuery({ query: mobile });
+  const isTablet = useMediaQuery({ query: tablet });
+  const isDesktop = useMediaQuery({ query: desktop });
 
   return { isMobile, isTablet, isDesktop };
 };
