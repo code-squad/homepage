14125a28d9116617f20de39692dcfc4ff7000505
Angelo <valrentin.angelo@gmail.com>
2022-08-26 16:58:42 +0900

    :hammer: window의 기본 innerWidth를 설정할 수 있도록 props추가

--- src/lib/testUtils/TestProvider.tsx
+++ src/lib/testUtils/TestProvider.tsx
@@ -2,8 +2,10 @@ import React from "react";
 // Theme
 import GlobalTheme from "lib/context/GlobalTheme";
 
-const TestProvider = ({ children }: { children: React.ReactNode }) => (
-  <GlobalTheme>{children}</GlobalTheme>
-);
+const TestProvider = ({ width, children }: { width?: number; children: React.ReactNode }) => {
+  global.innerWidth = width || 1440;
+
+  return <GlobalTheme>{children}</GlobalTheme>;
+};
 
 export default TestProvider;
