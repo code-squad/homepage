9c00069a44c26606eedd4be0cda80c7cc84ed0e9
HTMLhead <mrdoo309@gmail.com>
2022-08-30 13:34:27 +0900

    :lipstick: 하단 문구 스타일 수정

--- src/components/HomeGlobalNavigationBar/MobileNavigationList/MobileNavigationList.tsx
+++ src/components/HomeGlobalNavigationBar/MobileNavigationList/MobileNavigationList.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-// Libs
-import { MESSAGE } from "assets/static/phrases";
 import styled from "styled-components";
 import { Link } from "gatsby";
+// Typography
 import { Typography } from "typography";
+// Assets
+import { MESSAGE } from "assets/static/phrases";
 import { EXTERNAL } from "assets/static/urls";
+// Libs
 import { getCurrentPath } from "lib/utils";
 
 const MobileNavigationList: React.FC<{
@@ -12,39 +14,32 @@ const MobileNavigationList: React.FC<{
   open: boolean;
 }> = ({ links, open }) => {
   const currentPath = getCurrentPath();
+  const currentFirstPath = currentPath.split("/")[1];
 
   return (
     <NavigationListWrapper {...{ open }}>
       <ButtonList>
-        {links.map(({ title, path }) => {
-          const currentFirstPath = currentPath.split("/")[1];
-          const firstPath = path.split("/")[1];
-
-          return (
-            <li key={title}>
-              <LinkButton
-                selected={currentPath === path || currentFirstPath === firstPath}
-                to={path}
-              >
-                {title}
-              </LinkButton>
-            </li>
-          );
-        })}
+        {links.map(({ title, path }) => (
+          <li key={title}>
+            <LinkButton
+              selected={currentPath === path || currentFirstPath === path.split("/")[1]}
+              to={path}
+            >
+              {title}
+            </LinkButton>
+          </li>
+        ))}
       </ButtonList>
       <CompanyInfoWrapper>
         <Typography type="SBody">{MESSAGE.COPYRIGHT}</Typography>
-        <div>
-          <Typography type="SBody">{MESSAGE.COMPANY_EMAIL}</Typography>
-          <ExternalLink
-            href={`mailto:${EXTERNAL.EMAIL}`}
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-            underline
-          >
-            {MESSAGE.COMPANY_EMAIL_ADDRESS}
-          </ExternalLink>
-        </div>
+        <ExternalLink
+          href={`mailto:${EXTERNAL.EMAIL}`}
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+          underline
+        >
+          {MESSAGE.COMPANY_EMAIL_ADDRESS}
+        </ExternalLink>
         <Typography type="SBody">{MESSAGE.COMPANY_TEL_NUMBER}</Typography>
         <Typography type="SBody">{MESSAGE.COMPANY_ADDRESS}</Typography>
       </CompanyInfoWrapper>
@@ -76,19 +71,10 @@ const ButtonList = styled.ul`
 `;
 
 const ExternalLink = styled.a<{ underline?: boolean }>`
-  color: ${({ theme: { color } }) => color.greyScale.grey3};
   text-decoration: none;
-  text-decoration: ${({ underline }) => (underline ? "underline" : "none")};
-  &:hover {
-    text-decoration: underline;
-  }
-  @media ${({ theme }) => theme.device.mobile} {
-    font-size: ${({ theme: { fontSize } }) => fontSize.body.sm};
-  }
-  @media ${({ theme }) => theme.device.desktop} {
-    color: ${({ theme: { color } }) => color.white};
-    font-size: ${({ theme: { fontSize } }) => fontSize.body.xs};
-  }
+  color: ${({ theme: { color } }) => color.greyScale.grey1};
+  font-size: ${({ theme: { fontSize } }) => fontSize.body.sm};
+  line-height: ${({ theme: { lineHeight } }) => lineHeight.body.sm};
 `;
 
 const LinkButton = styled(Link)<{ selected?: boolean }>`
@@ -96,18 +82,16 @@ const LinkButton = styled(Link)<{ selected?: boolean }>`
   font-size: ${({ theme: { fontSize } }) => fontSize.bold.xl};
   font-weight: ${({ selected, theme: { fontWeight } }) =>
     selected ? fontWeight.medium : fontWeight.regular};
-  line-height: ${({ theme: { lineHeight } }) => lineHeight.body.sm};
+  line-height: ${({ theme: { lineHeight } }) => lineHeight.bold.xl};
   letter-spacing: ${({ theme: { letterSpacing } }) => letterSpacing};
   text-decoration: ${({ selected }) => (selected ? "underline" : "none")};
-  &:hover {
-    text-decoration: underline;
-  }
 `;
 
 const CompanyInfoWrapper = styled.ul`
   display: flex;
   flex-direction: column;
-  width: 80%;
+  padding: 0 2.4rem;
+  color: ${({ theme: { color } }) => color.greyScale.grey1};
 `;
 
 export default MobileNavigationList;
