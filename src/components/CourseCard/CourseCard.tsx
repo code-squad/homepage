b0b3d73e44794ea3017f01337861c3380cdaf0af
HTMLhead <mrdoo309@gmail.com>
2022-08-31 15:51:26 +0900

    :lipstick: 코스 카드에 태그 컴포넌트가 적용되며 수정되는 css적용

--- src/components/CourseCard/CourseCard.tsx
+++ src/components/CourseCard/CourseCard.tsx
@@ -71,12 +71,6 @@ const CourseInfoWrapper = styled.div`
   flex-direction: column;
   @media ${({ theme }) => theme.device.mobile} {
     padding: 2rem 0 2rem 1.6rem;
-    & > *:nth-child(2) {
-      margin-bottom: 0.4rem;
-    }
-    & > *:nth-child(3) {
-      margin-bottom: 0.4rem;
-    }
   }
   @media ${({ theme }) => theme.device.tablet} {
     padding: 0.5rem 0 0.5rem 2.4rem;
@@ -89,10 +83,20 @@ const CourseInfoWrapper = styled.div`
   }
   @media ${({ theme }) => theme.device.desktop} {
     padding: 3.45rem 0 3.45rem 2.4rem;
+    & > *:nth-child(2) {
+      margin-bottom: 0.4rem;
+    }
+    & > *:nth-child(3) {
+      margin-bottom: 0.4rem;
+    }
   }
 `;
 
 const CardImg = styled.img`
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 13.4rem;
+    height: 13.4rem;
+  }
   @media ${({ theme }) => theme.device.tablet} {
     width: 13.4rem;
     height: 13.4rem;
@@ -105,21 +109,6 @@ const CardImg = styled.img`
 
 const TagList = styled.ul`
   display: flex;
-  margin-top: 1.6rem;
-  & > *:not(:last-child) {
-    margin-right: 0.8rem;
-  }
-  color: ${({ theme: { color } }) => color.black};
-`;
-
-const CourseTagItem = styled.div`
-  width: 4.2rem;
-  height: 2.6rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({ theme: { color } }) => color.secondary.blue3};
-  border-radius: 99.9rem;
 `;
 
 export default CourseCard;
