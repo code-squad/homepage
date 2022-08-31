542f6008356b0e77285340722d1858a9d6ab6df3
HTMLhead <mrdoo309@gmail.com>
2022-08-31 15:35:17 +0900

    :recycle: SButton 컴포넌트를 tag컴포넌트로 수정 및 링크 기능 제거

--- src/components/Tag/Tag.test..tsx
+++ src/components/Tag/Tag.test..tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 // Testing-Component
-import { SButton } from ".";
+import { Tag } from ".";
 // Assets
 import { LINK } from "assets/static/phrases";
 import { INTERNAL } from "assets/static/urls";
@@ -11,24 +11,17 @@ import { TestProvider } from "lib/testUtils";
 describe("<SButton>", () => {
   const props = {
     type: "Black" as "Black" | "Orange" | "Green",
-    to: INTERNAL.SUBSCRIPTION,
-    children: "test",
+    text: "test",
   };
   const renderSButton = (buttonText: string) =>
     render(
       <TestProvider>
-        <SButton {...props}>{buttonText}</SButton>
+        <Tag {...props}>{buttonText}</Tag>
       </TestProvider>
     );
   it("버튼 문구가 보여진다.", () => {
     const { getByText } = renderSButton(LINK.SUBSCRIPTION);
 
     getByText(LINK.SUBSCRIPTION);
-  });
-  it("버튼을 클릭하면 props로 전달된곳으로 페이지가 이동된다.", () => {
-    const { getByRole } = renderSButton(LINK.SUBSCRIPTION);
-
-    const linkEle = getByRole("link", { name: LINK.SUBSCRIPTION });
-    expect(linkEle?.getAttribute("href")).toBe(INTERNAL.SUBSCRIPTION);
   });
 });
