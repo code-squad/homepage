cea6fe2b198bfb6218ba838f46c2a7543aaa2e31
HTMLhead <mrdoo309@gmail.com>
2022-08-31 13:03:13 +0900

    :white_check_mark: 코스 리스트 컴포넌트가 mdx 파일을 사용하도록 테스트코드 수정

--- src/pageComponents/code-together/CourseList/CourseList.test.tsx
+++ src/pageComponents/code-together/CourseList/CourseList.test.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import * as Gatsby from "gatsby";
 import { render } from "@testing-library/react";
+// Type
+import { CourseListType } from "@type/Course";
 // Testing-Component
 import { CourseList } from ".";
-// Assets
-import { LINK, LINK_DESCRIPTION } from "assets/static/phrases";
-import { EXTERNAL, INTERNAL } from "assets/static/urls";
+// Mocks
+import { CourseListQueryResult } from "./CourseList.test.mock";
 // Libs
 import { TestProvider } from "lib/testUtils";
+import { strainMdxInfo } from "lib/utils";
 
 describe("<CourseList>", () => {
   const renderCourseList = () =>
@@ -15,20 +18,28 @@ describe("<CourseList>", () => {
         <CourseList />
       </TestProvider>
     );
+
+  const useStaticQuery = jest.spyOn(Gatsby, "useStaticQuery");
+  useStaticQuery.mockImplementation(() => CourseListQueryResult);
+  const { courses }: { courses: CourseListType[] } = strainMdxInfo(CourseListQueryResult);
+  const javascriptCourse = courses[0];
+  const iOSCourse = courses[1];
   it("자바스크립트 코스 링크가 보여진다.", async () => {
     const { getByText, getAllByRole } = renderCourseList();
-    getByText(LINK.JAVASCRIPT);
-    getByText(LINK_DESCRIPTION.JAVASCRIPT);
+    const { title, path, description } = javascriptCourse;
+    getByText(title);
+    getByText(description);
 
     const [javascriptLink] = getAllByRole("link");
-    expect(javascriptLink?.getAttribute("href")).toBe(INTERNAL.JAVASCRIPT);
+    expect(javascriptLink?.getAttribute("href")).toBe(path);
   });
-  it("코드투게더 링크가 보여진다.", async () => {
+  it("iOS 코스 링크가 보여진다.", async () => {
     const { getByText, getAllByRole } = renderCourseList();
-    getByText(LINK.CLEAN_SWIFT);
-    getByText(LINK_DESCRIPTION.CLEAN_SWIFT);
+    const { title, path, description } = iOSCourse;
+    getByText(title);
+    getByText(description);
 
     const [_, codeTogetherLink] = getAllByRole("link");
-    expect(codeTogetherLink?.getAttribute("href")).toBe(EXTERNAL.CLEAN_SWIFT);
+    expect(codeTogetherLink?.getAttribute("href")).toBe(path);
   });
 });
