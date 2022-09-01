e08b1b8cffa573a976b4fee0085fbd9b519e1189
HTMLhead <mrdoo309@gmail.com>
2022-09-01 14:27:13 +0900

    :lipstick: studyFeature컴포넌트 반응형 적용

--- src/pageComponents/code-together/StudyFeature/StudyFeature.tsx
+++ src/pageComponents/code-together/StudyFeature/StudyFeature.tsx
@@ -16,7 +16,7 @@ const StudyFeature: React.FC = () => {
   const data = useStaticQuery(CodeTogetherStudyFeatureQuery);
   const { studyFeatures }: { studyFeatures: CodeTogetherFeatureType[] } = strainMdxInfo(data);
 
-  const { isDesktop } = useResponsive();
+  const { isDesktop, isMobile } = useResponsive();
 
   return (
     <StudyWrapper>
@@ -24,7 +24,12 @@ const StudyFeature: React.FC = () => {
       <FeatureList>
         {studyFeatures.map(({ title, descriptions, img }) => (
           <FeatureItem key={title}>
-            <ImageCard {...{ descriptions, title }} img={features[img]} medium={isDesktop} />
+            <ImageCard
+              {...{ descriptions, title }}
+              img={features[img]}
+              medium={isDesktop}
+              vertical={isMobile}
+            />
           </FeatureItem>
         ))}
       </FeatureList>
@@ -35,34 +40,43 @@ const StudyFeature: React.FC = () => {
 const StudyWrapper = styled.div`
   padding-top: 18rem;
   margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: space-between;
-  & > *:not(:last-child) {
-    margin-bottom: 4rem;
-  }
+  margin-top: 8rem;
   @media ${({ theme }) => theme.device.mobile} {
-    width: 31.2rem;
+    padding: 0 2.4rem;
   }
   @media ${({ theme }) => theme.device.tablet} {
-    width: 60.8rem;
+    padding: 0 8rem;
   }
   @media ${({ theme }) => theme.device.desktop} {
     width: 106.2rem;
+    padding: 0 18.9rem;
   }
 `;
 
 const FeatureList = styled.ul`
-  width: 106.2rem;
-  margin-top: 6.4rem;
-  display: flex;
-  flex-flow: row wrap;
-  align-content: flex-start;
-  & > *:not(:nth-child(2n)) {
-    margin-right: 13.3rem;
+  margin-top: 8rem;
+  @media ${({ theme }) => theme.device.mobile} {
+    margin-top: 4.8rem;
+    display: flex;
+    flex-direction: column;
+    & > *:not(:last-child) {
+      margin-bottom: 4.8rem;
+    }
   }
-  & > *:not(:nth-last-child(-n + 2)) {
-    margin-bottom: 8rem;
+  @media ${({ theme }) => theme.device.tablet} {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 0 7.9rem;
+    & > *:nth-child(2n) {
+      margin-bottom: 8rem;
+    }
+  }
+  @media ${({ theme }) => theme.device.desktop} {
+    width: 106.2rem;
+    & > *:not(:nth-child(2n)) {
+      margin-right: 13.3rem;
+      margin-bottom: 8.4rem;
+    }
   }
 `;
 
