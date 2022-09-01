284a72207217f972b79c39165068e7a0941cfed2
Angelo <valrentin.angelo@gmail.com>
2022-09-01 19:04:36 +0900

    :lipstick: 팀 문화 페이지에 반응형 모바일/태블릿 UI 적용, Typography 리팩터링

--- src/pageComponents/team-culture/TeamIntroduce/TeamIntroduce.tsx
+++ src/pageComponents/team-culture/TeamIntroduce/TeamIntroduce.tsx
@@ -1,59 +1,73 @@
 import React from "react";
 import styled from "styled-components";
 // Typography
-import { LDisplay, HLBold } from "typography";
+import { Typography } from "typography";
 // Assets
+import header from "assets/img/illusts/header";
 import { TITLE, SUBTITLE } from "assets/static/phrases";
+// Libs
+import { useResponsive } from "lib/hooks";
 
 const TeamIntroduce: React.FC = () => {
+  const { isMobile } = useResponsive();
+
   return (
     <TeamIntroduceWrapper>
-      <Background />
       <IntroduceText>
-        <LDisplay>{SUBTITLE.WE}</LDisplay>
-        <HLBold>{TITLE.TEAM_CULTURE}</HLBold>
+        {isMobile ? (
+          <>
+            <Typography type="SDisplay">{SUBTITLE.WE}</Typography>
+            <Typography type="MBody" style={{ marginTop: "1.6rem" }}>
+              {TITLE.TEAM_CULTURE}
+            </Typography>
+          </>
+        ) : (
+          <Typography type="SDisplay">{SUBTITLE.WE + " " + TITLE.TEAM_CULTURE}</Typography>
+        )}
       </IntroduceText>
     </TeamIntroduceWrapper>
   );
 };
 
-const Background = styled.div`
-  position: absolute;
-  z-index: -1;
-  background-color: ${({ theme: { color } }) => color.secondary.blue3};
-  height: 56rem;
-  width: 100%;
-  min-width: 144rem;
+const TeamIntroduceWrapper = styled.div`
+  position: relative;
   display: flex;
-  align-items: center;
+  align-items: flex-end;
   justify-content: center;
-  &::after {
-    content: "";
-    display: block;
-    width: 28rem;
-    height: 28rem;
-    background-color: #b2dee6;
-    position: relative;
-    left: 32rem;
-    transform: rotate(45deg);
+  color: ${({ theme: { color } }) => color.black};
+  background-color: ${({ theme: { color } }) => color.secondary.blue3};
+  background-repeat: no-repeat;
+  background-position: right;
+  @media ${({ theme }) => theme.device.mobile} {
+    height: 35.2rem;
+    background-image: ${`url(${header.mobilePattern2})`};
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    height: 44.8rem;
+    background-image: ${`url(${header.tabletPattern2})`};
+  }
+  @media ${({ theme }) => theme.device.desktop} {
+    min-width: 144rem;
+    min-height: 56rem;
+    background-image: ${`url(${header.desktopPattern2})`};
   }
 `;
 
-const TeamIntroduceWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  position: relative;
-  width: 100%;
-  min-width: 144rem;
-  min-height: 56rem;
-  color: ${({ theme: { color } }) => color.black};
-  white-space: pre-line;
-  text-align: start;
-`;
-
 const IntroduceText = styled.div`
-  width: 106.2rem;
-  margin: 0 auto;
+  @media ${({ theme }) => theme.device.mobile} {
+    padding: 0 2.4rem;
+    padding-bottom: 4rem;
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    padding: 0 8rem;
+    padding-bottom: 5.6rem;
+  }
+  @media ${({ theme }) => theme.device.desktop} {
+    width: 106.2rem;
+    padding: 0 18.9rem;
+    padding-bottom: 8rem;
+    margin: 0 auto;
+  }
 `;
 
 export default TeamIntroduce;
