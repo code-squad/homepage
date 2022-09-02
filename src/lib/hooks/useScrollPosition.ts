c4ff1e79e37da723c6cf45634d040cbed4ee905a
Angelo <valrentin.angelo@gmail.com>
2022-09-03 00:01:35 +0900

    :bug: 쓰로틀링이 제대로 동작하지 않았던 문제 해결

--- src/lib/hooks/useScrollPosition.ts
+++ src/lib/hooks/useScrollPosition.ts
@@ -2,21 +2,23 @@ import React from "react";
 
 const useScrollPosition = () => {
   const [scrollPosition, setScrollPosition] = React.useState(false);
-  const [throttle, setThrottle] = React.useState(false);
+  const throttle = React.useRef(false);
 
   const updatePosition = () => {
-    if (throttle) return;
+    if (throttle.current && window.pageYOffset > 0) return;
 
     const edgeYOffset = 15;
     if (window.pageYOffset > edgeYOffset) setScrollPosition(true);
     else if (window.pageXOffset <= edgeYOffset) setScrollPosition(false);
 
+    throttle.current = true;
     setTimeout(async () => {
-      setThrottle(true);
+      throttle.current = false;
     }, 100);
   };
 
   React.useEffect(() => {
+    updatePosition();
     window.addEventListener("scroll", updatePosition);
     return () => window.removeEventListener("scroll", updatePosition);
   }, []);
