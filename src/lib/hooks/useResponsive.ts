04ed3733d176a110642a089f46e3a5c2225885cf
Angelo <valrentin.angelo@gmail.com>
2022-09-03 00:02:49 +0900

    :bug: typeof window 조건문에 의하여 모바일 환경에서 글자가 줄어드는것이 보이는 현상 해결

--- src/lib/hooks/useResponsive.ts
+++ src/lib/hooks/useResponsive.ts
@@ -2,19 +2,15 @@ import React from "react";
 import theme from "styles/theme";
 
 function useResponsive() {
-  if (typeof window === "undefined") return { isMobile: false, isTablet: false, isDesktop: true };
-
   const { breakPoint } = theme;
 
-  const [windowWidthSize, setWindowWidthSize] = React.useState(window.innerWidth);
+  const [windowWidthSize, setWindowWidthSize] = React.useState(breakPoint.mobile);
 
   React.useEffect(() => {
     const handleResize = () => {
       setWindowWidthSize(window.innerWidth);
     };
     window.addEventListener("resize", handleResize);
-    handleResize();
-
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
