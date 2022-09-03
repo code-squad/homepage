4f23e4778b6e6e37ac18c18cb13f74f91ea8c619
Angelo <valrentin.angelo@gmail.com>
2022-09-03 22:10:46 +0900

    :bug: 최초 렌더링시 글자 크기가 바뀌는 현상 수정

--- src/lib/hooks/useResponsive.ts
+++ src/lib/hooks/useResponsive.ts
@@ -1,13 +1,50 @@
 import React from "react";
 import theme from "styles/theme";
 
+interface Responsive {
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
+}
+
 function useResponsive() {
   const { breakPoint } = theme;
 
-  const [windowWidthSize, setWindowWidthSize] = React.useState(breakPoint.mobile);
+  const [responsive, setResponsive] = React.useState<Responsive>({
+    isMobile: false,
+    isTablet: false,
+    isDesktop: false,
+  });
+  const throttle = React.useRef(false);
+
+  const getCurrentResponsive = () => {
+    const windowWidthSize = window.innerWidth;
+    let [isMobile, isTablet, isDesktop] = [false, false, false];
+
+    if (windowWidthSize < breakPoint.tablet) isMobile = true;
+    if (breakPoint.tablet <= windowWidthSize && windowWidthSize < breakPoint.desktop)
+      isTablet = true;
+    if (windowWidthSize >= breakPoint.desktop) isDesktop = true;
+
+    return { isMobile, isTablet, isDesktop };
+  };
+
+  const checkResponsiveChanged = (prevResponsive: Responsive, currResponsive: Responsive) => {
+    if (JSON.stringify(prevResponsive) === JSON.stringify(currResponsive)) return false;
+    return true;
+  };
 
   const handleResize = () => {
-    setWindowWidthSize(window.innerWidth);
+    const currentResponsive = getCurrentResponsive();
+
+    const responsiveChanged = checkResponsiveChanged(responsive, currentResponsive);
+    if (throttle.current || !responsiveChanged) return;
+
+    setResponsive(currentResponsive);
+    throttle.current = true;
+    setTimeout(async () => {
+      throttle.current = false;
+    }, 100);
   };
 
   React.useEffect(() => {
@@ -16,11 +53,7 @@ function useResponsive() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  let [isMobile, isTablet, isDesktop] = [false, false, false];
-  if (windowWidthSize < breakPoint.tablet) isMobile = true;
-  if (breakPoint.tablet <= windowWidthSize && windowWidthSize < breakPoint.desktop) isTablet = true;
-  if (windowWidthSize >= breakPoint.desktop) isDesktop = true;
-  return { isMobile, isTablet, isDesktop };
+  return responsive;
 }
 
 export default useResponsive;
