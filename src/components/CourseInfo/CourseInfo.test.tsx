1026145bb845961f9c130adf72129110819d4afe
HTMLhead <mrdoo309@gmail.com>
2022-09-16 17:09:04 +0900

    :fire: subCourse관련 문구 제거

--- src/components/CourseInfo/CourseInfo.test.tsx
+++ src/components/CourseInfo/CourseInfo.test.tsx
@@ -16,12 +16,7 @@ describe("<CourseInfo>", () => {
     { title: "6개월", content: "", img: "calendar" as keyof typeof icons },
     { title: "매달 55만원", content: "6개월 기준 총 396만원", img: "coin" as keyof typeof icons },
   ];
-  const subCourseInfos = [
-    { title: "마스터즈", content: "", img: "" as keyof typeof icons },
-    { title: "6개월", content: "", img: "calendar" as keyof typeof icons },
-    { title: "매달 55만원", content: "6개월 기준 총 396만원", img: "coin" as keyof typeof icons },
-  ];
-  const props = (subCourse: boolean) => ({
+  const props = {
     backgroundImage: header.desktopPattern3,
     backgroundColor: theme.color.primary.green4,
     title: "마스터즈 코스",
@@ -32,29 +27,29 @@ describe("<CourseInfo>", () => {
       "6개월 이상 프로그래밍을 공부해보았고, 동료와 함께 성장하고 싶은 사람",
       "실무 프로그래머로 가는 길이 궁금한 사람",
     ],
-    courseInfos: subCourse ? subCourseInfos : courseInfos,
-  });
+    courseInfos,
+  };
   const renderCourseInfo = () =>
     render(
       <TestProvider>
-        <CourseInfo {...props(false)} />
+        <CourseInfo {...props} />
       </TestProvider>
     );
   it("코스 제목이 보여진다.", async () => {
     const { getByText } = renderCourseInfo();
-    const { title } = props(false);
+    const { title } = props;
 
     getByText(title);
   });
   it("코스 설명이 보여진다.", async () => {
     const { getByText } = renderCourseInfo();
-    const { description } = props(false);
+    const { description } = props;
 
     getByText(description);
   });
   it("코스 금액/기간 등의 정보들이 보여진다.", async () => {
     const { getByText, getByAltText } = renderCourseInfo();
-    const { courseInfos } = props(false);
+    const { courseInfos } = props;
 
     for (const { title, content } of courseInfos) {
       getByAltText(`course-info-img-${title}`);
@@ -70,24 +65,10 @@ describe("<CourseInfo>", () => {
   });
   it("교육 과정 대상자 내용들이 보여진다.", async () => {
     const { getByText } = renderCourseInfo();
-    const { targets } = props(false);
+    const { targets } = props;
 
     for (const target of targets) {
       getByText(target);
     }
-  });
-
-  const renderSubCourseInfo = () =>
-    render(
-      <TestProvider>
-        <CourseInfo {...props(true)} subCourse />
-      </TestProvider>
-    );
-
-  it("subCourse가 true인 경우 문구만 보여지는 텍스트가 있다.", async () => {
-    const { getByText } = renderSubCourseInfo();
-    const { courseInfos } = props(true);
-
-    getByText(courseInfos[0].title)
   });
 });
