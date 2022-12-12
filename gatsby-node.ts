fb03575ffd899c04667869a2c633050f4dde5992
HTMLhead <mrdoo309@gmail.com>
2022-12-12 16:43:28 +0900

    :sparkles: 다이나믹하게 페이지를 만드는 gatsby-node파일 제작

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,13 +1,50 @@
-exports.createPages = async ({ graphql, actions }) => {
-  const { data } = await graphql(`
-    query slug {
+import path from "path";
+import { type GatsbyNode } from "gatsby";
+
+const createPages: GatsbyNode["createPages"] = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const { data } = await graphql<{ allMdx: any }>(`
+    query {
       allMdx {
         nodes {
-          slug
+          frontmatter {
+            templateKey
+            path
+          }
         }
       }
     }
   `);
 
-  console.log(data.allMdx.nodes.filter((v) => v.slug.match("code-together/course/")));
+  if (!data) return;
+
+  const pathArr = data.allMdx.nodes.filter(({ frontmatter }: { frontmatter: any }) => {
+    return frontmatter?.path?.match("code-together/course/");
+  });
+
+  const onlyPathArr: string[] = pathArr.map(({ frontmatter }: { frontmatter: any }) => {
+    return frontmatter.path;
+  });
+
+  const slugList: string[] = [...new Set(onlyPathArr)];
+
+  const codeTogetherTemplate = path.resolve("src/template/codeTogetherTemplate.tsx");
+
+  slugList.forEach((slug: string) => {
+    const locationArr = slug.split("/");
+    const courseName = locationArr[locationArr.length - 1];
+    createPage({
+      path: `/${slug}`,
+      component: codeTogetherTemplate,
+      context: {
+        masthead: `codeTogether_${courseName}_masthead`,
+        registration: `codeTogether_${courseName}_plan`,
+        deatilCurriculum: `codeTogether_${courseName}_part*`,
+        timetable: `codeTogether_${courseName}_plan`,
+        graduateReview: `codeTogether_${courseName}_reviews`,
+      },
+    });
+  });
 };
+
+export { createPages };
