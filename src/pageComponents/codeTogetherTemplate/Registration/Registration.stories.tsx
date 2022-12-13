234768f17ada2501bf4140d4e85bc3642d661fde
HTMLhead <mrdoo309@gmail.com>
2022-12-13 15:00:20 +0900

    :sparkles: 템플릿 컴포넌트별 스토리 제작

--- src/pageComponents/codeTogetherTemplate/Registration/Registration.stories.tsx
+++ src/pageComponents/codeTogetherTemplate/Registration/Registration.stories.tsx
@@ -6,7 +6,7 @@ import { Registration } from ".";
 import RegistrationDocs from "./Registration.docs.mdx";
 
 export default {
-  title: "pagecomponent/code-together/javascript/Registration",
+  title: "pagecomponent/template/code-together/Registration",
   component: Registration,
   parameters: {
     docs: {
@@ -15,6 +15,16 @@ export default {
   },
 } as ComponentMeta<typeof Registration>;
 
-const Template: ComponentStory<typeof Registration> = () => <Registration />;
+const Template: ComponentStory<typeof Registration> = (args) => <Registration {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  registrations: [
+    {
+      title: "코드투게더 클린 프론트엔드 과정 대기자 신청하기",
+      description: "",
+      caption: "교육 기간: 2023년 1월~4월 사이",
+      path: "https://docs.google.com/forms/d/e/1FAIpQLSeMe3ql6Ifi9FpHy1yqSrasSq8lqgbhdEvrOhQEP6KB2kwTDA/viewform",
+    },
+  ],
+};
